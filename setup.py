7380973e311524dc7aaddefadbe5e525b7a6a6b3
Jeremy Haberman <jeremy.haberman@code42.com>
2019-05-07 13:11:12 -0500

    Merge pull request #42 in SH/lib_c42_python_sdk from jphaberman/INTEG-204-tests-in-package to master
    Squashed commit of the following:
    
    commit 869382bc0b139d7dc61727458345cf3cc31f709d
    Author: Jeremy Haberman <jeremy.haberman@code42.com>
    Date:   Tue May 7 09:25:12 2019 -0500
    
        INTEG-204 Include only py42 and py42.* packages in distribution

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="py42",
     version="0.1.2",
     description="Official Code42 API Client",
-    packages=find_packages(),
+    packages=find_packages(include=["py42", "py42.*"]),
     python_requires=">=2.7, <3",
     install_requires=["requests>=2.3"],
     license="MIT",

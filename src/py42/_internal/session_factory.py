71313a639372184e0302762cbb0ce2b12b1bc3bc
Alan Grgic <alan.grgic@code42.com>
2020-02-11 11:55:45 -0600

    clearer var name

--- src/py42/_internal/session_factory.py
+++ src/py42/_internal/session_factory.py
@@ -42,8 +42,8 @@ class SessionFactory(object):
         header_modifier = self._session_modifier_factory.create_v3_session_modifier()
         return self._create_session(self._session_impl, file_event_login_provider, header_modifier)
 
-    def create_key_value_store_session(self, key_value_store_locator):
-        return self._create_session(self._session_impl, key_value_store_locator)
+    def create_key_value_store_session(self, key_value_store_login_provider):
+        return self._create_session(self._session_impl, key_value_store_login_provider)
 
     def _create_session(self, session_impl, login_provider, modifier=None):
         handler = None

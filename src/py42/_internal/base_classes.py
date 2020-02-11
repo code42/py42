3ef15a5750038a288b710759406c6c98c307c987
Juliya Smith <juliya.smith@code42.com>
2020-02-11 21:09:26 +0000

    Remove other base clients

--- src/py42/_internal/base_classes.py
+++ src/py42/_internal/base_classes.py
@@ -1,19 +1,9 @@
+class BaseClient(object):
+    def __init__(self, default_session):
+        self._default_session = default_session
+
+
 class BaseAuthorityClient(object):
     def __init__(self, default_session, v3_required_session):
         self._default_session = default_session
         self._v3_required_session = v3_required_session
-
-
-class BaseStorageClient(object):
-    def __init__(self, session):
-        self._session = session
-
-
-class BaseFileEventClient(object):
-    def __init__(self, session):
-        self._session = session
-
-
-class BaseEmployeeCaseManagementClient(object):
-    def __init__(self, session):
-        self._session = session
--- src/py42/_internal/clients/employee_case_management/departing_employee.py
+++ src/py42/_internal/clients/employee_case_management/departing_employee.py
@@ -1,6 +1,6 @@
 import json
 
-from py42._internal.base_classes import BaseEmployeeCaseManagementClient
+from py42._internal.base_classes import BaseClient
 from py42.util import get_obj_from_response
 
 
@@ -11,7 +11,7 @@ class DepartingEmployeeFilterType(object):
     EXFILTRATION_30_DAYS = "EXFILTRATION_30_DAYS"
 
 
-class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
+class DepartingEmployeeClient(BaseClient):
     _base_uri = "/svc/api/v1/departingemployee/"
     _tenant_id = None
 
@@ -39,13 +39,13 @@ class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
             u"cloudUsernames": cloud_usernames,
         }
         uri = u"{0}create".format(self._base_uri)
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def resolve_departing_employee(self, case_id, tenant_id=None):
         tenant_id = tenant_id if tenant_id else self._get_current_tenant_id()
         uri = u"{0}resolve".format(self._base_uri)
         data = {u"caseId": case_id, u"tenantId": tenant_id}
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def get_all_departing_employees(
         self,
@@ -66,7 +66,7 @@ class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
             u"srtKey": sort_key,
             u"srtDirection": sort_direction,
         }
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def search_departing_employees(
         self,
@@ -89,13 +89,13 @@ class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
             u"srtKey": sort_key,
             u"srtDirection": sort_direction,
         }
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def toggle_alerts(self, tenant_id=None, alerts_enabled=True):
         tenant_id = tenant_id if tenant_id else self._get_current_tenant_id()
         uri = self._get_uri(u"togglealerts")
         data = {u"tenantId": tenant_id, u"alertsEnabled": alerts_enabled}
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def get_case_by_username(self, username, tenant_id=None):
         tenant_id = tenant_id if tenant_id else self._get_current_tenant_id()
@@ -106,7 +106,7 @@ class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
         tenant_id = tenant_id if tenant_id else self._get_current_tenant_id()
         uri = self._get_uri(u"details")
         data = {u"tenantId": tenant_id, u"caseId": case_id}
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def update_case(
         self,
@@ -135,7 +135,7 @@ class DepartingEmployeeClient(BaseEmployeeCaseManagementClient):
             u"status": status,
             u"cloudUsernames": cloud_usernames,
         }
-        return self._session.post(uri, data=json.dumps(data))
+        return self._default_session.post(uri, data=json.dumps(data))
 
     def _get_uri(self, resource_name):
         return u"{0}{1}".format(self._base_uri, resource_name)

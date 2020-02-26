70f5ecb6dfcc617fa1c62f845560b86c67ba0aa4
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-02-26 17:09:12 +0530

    INTEG-892-Refactor : Rename legal hold methods

--- tests/_internal/clients/test_legal_hold.py
+++ tests/_internal/clients/test_legal_hold.py
@@ -19,94 +19,92 @@ DEFAULT_GET_LEGAL_HOLDS_PARAMS = {
     "q": None,
 }
 
-MOCK_GET_LEGAL_HOLDS_RESPONSE = """{
-  "data": {"legalHolds":["foo"]} 
+MOCK_GET_ALL_MATTERS_RESPONSE = """{
+  "data": {"legalHolds":["foo"]}
 }"""
 
-MOCK_EMPTY_GET_LEGAL_HOLDS_RESPONSE = """{
-  "data": {"legalHolds":[]} 
+MOCK_EMPTY_GET_ALL_MATTERS_RESPONSE = """{
+  "data": {"legalHolds":[]}
 }"""
 
-MOCK_GET_LEGAL_HOLD_MEMBERSHIP_RESPONSE = """{
-  "data": {"legalHoldMemberships":["foo"]} 
+MOCK_GET_ALL_MATTER_CUSTODIANS_RESPONSE = """{
+  "data": {"legalHoldMemberships":["foo"]}
 }"""
 
-MOCK_EMPTY_GET_LEGAL_HOLD_MEMBERSHIP_RESPONSE = """{
-  "data": {"legalHoldMemberships":[]} 
+MOCK_EMPTY_GET_ALL_MATTER_CUSTODIANS_RESPONSE = """{
+  "data": {"legalHoldMemberships":[]}
 }"""
 
 
 class TestLegalHoldClient(object):
     @pytest.fixture
-    def mock_get_legal_holds_response(self, mocker):
+    def mock_get_all_matters_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
-        response.text = MOCK_GET_LEGAL_HOLDS_RESPONSE
+        response.text = MOCK_GET_ALL_MATTERS_RESPONSE
         return response
 
     @pytest.fixture
-    def mock_get_legal_holds_empty_response(self, mocker):
+    def mock_get_all_matters_empty_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
-        response.text = MOCK_EMPTY_GET_LEGAL_HOLDS_RESPONSE
+        response.text = MOCK_EMPTY_GET_ALL_MATTERS_RESPONSE
         return response
 
     @pytest.fixture
-    def mock_get_legal_hold_memberships_response(self, mocker):
+    def mock_get_all_matter_custodians_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
-        response.text = MOCK_GET_LEGAL_HOLD_MEMBERSHIP_RESPONSE
+        response.text = MOCK_GET_ALL_MATTER_CUSTODIANS_RESPONSE
         return response
 
     @pytest.fixture
-    def mock_get_legal_hold_memberships_empty_response(self, mocker):
+    def mock_get_all_matter_custodians_empty_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
-        response.text = MOCK_EMPTY_GET_LEGAL_HOLD_MEMBERSHIP_RESPONSE
+        response.text = MOCK_EMPTY_GET_ALL_MATTER_CUSTODIANS_RESPONSE
         return response
 
-    def test_get_legal_hold_by_uid_calls_get_with_uri_and_params(
-        self, session, v3_required_session
-    ):
+    def test_get_matter_by_uid_calls_get_with_uri_and_params(self, session, v3_required_session):
         client = LegalHoldClient(session, v3_required_session)
-        client.get_legal_hold_by_uid("LEGAL_HOLD_UID")
+        client.get_matter_by_uid("LEGAL_HOLD_UID")
         uri = "{0}/{1}".format(LEGAL_HOLD_URI, "LEGAL_HOLD_UID")
         session.get.assert_called_once_with(uri)
 
-    def test_get_legal_holds_calls_get_expected_number_of_times(
+    def test_get_all_matters_calls_get_expected_number_of_times(
         self,
         session,
         v3_required_session,
-        mock_get_legal_holds_response,
-        mock_get_legal_holds_empty_response,
+        mock_get_all_matters_response,
+        mock_get_all_matters_empty_response,
     ):
         py42.settings.items_per_page = 1
         client = LegalHoldClient(session, v3_required_session)
         session.get.side_effect = [
-            mock_get_legal_holds_response,
-            mock_get_legal_holds_response,
-            mock_get_legal_holds_empty_response,
+            mock_get_all_matters_response,
+            mock_get_all_matters_response,
+            mock_get_all_matters_empty_response,
         ]
-        for page in client.get_legal_holds():
+        for page in client.get_all_matters():
             pass
         py42.settings.items_per_page = 1000
         assert session.get.call_count == 3
 
-    def test_get_legal_hold_memberships_calls_get_expected_number_of_times(
+    def test_get_all_matter_custodians_calls_get_expected_number_of_times(
         self,
         session,
         v3_required_session,
-        mock_get_legal_hold_memberships_response,
-        mock_get_legal_hold_memberships_empty_response,
+        mock_get_all_matter_custodians_response,
+        mock_get_all_matter_custodians_empty_response,
     ):
         py42.settings.items_per_page = 1
         client = LegalHoldClient(session, v3_required_session)
         session.get.side_effect = [
-            mock_get_legal_hold_memberships_response,
-            mock_get_legal_hold_memberships_response,
-            mock_get_legal_hold_memberships_empty_response,
+            mock_get_all_matter_custodians_response,
+            mock_get_all_matter_custodians_response,
+            mock_get_all_matter_custodians_empty_response,
         ]
-        for page in client.get_legal_hold_memberships():
+        for page in client.get_all_matter_custodians():
             pass
         py42.settings.items_per_page = 1000
         assert session.get.call_count == 3

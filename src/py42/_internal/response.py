933db0150a790b8d7e7879fd9449e4647e5f7236
Alan Grgic <alan.grgic@code42.com>
2020-02-28 15:00:50 -0600

    tweak

--- src/py42/_internal/response.py
+++ src/py42/_internal/response.py
@@ -2,21 +2,20 @@ import json
 
 
 class Py42Response(object):
-    def __init__(self, requests_respsonse, json_key):
-        self._response = requests_respsonse
+    def __init__(self, requests_response, json_key):
+        self._response = requests_response
         self._json_key = json_key
-        self._dict = None
+        response_dict = json.loads(self._response.text)
+        self._data_root = response_dict.get("data") or response_dict
 
     def __getitem__(self, key):
-        if not self._dict:
-            self._dict = json.loads(self._response.text)
-
-        data_root = self._dict.get("data") or self._dict
-        return data_root[key]
-
-    def __str__(self):
-        return self._response.text
+        item_root = self._data_root[self._json_key]
+        return item_root[key]
 
     @property
-    def raw(self):
+    def raw_json(self):
+        return json.dumps(self._data_root)
+
+    @property
+    def api_response(self):
         return self._response

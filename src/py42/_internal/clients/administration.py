1f6d4e3acbdc61badf6f9d2f8c332a483a5f512d
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-03 17:28:16 +0530

    INTEG-913 Changed return type to Py42Response for AdministrationClient

--- src/py42/_internal/clients/administration.py
+++ src/py42/_internal/clients/administration.py
@@ -1,17 +1,18 @@
 from py42._internal.base_classes import BaseAuthorityClient
+from py42._internal.response import Py42Response
 
 
 class AdministrationClient(BaseAuthorityClient):
     def get_diagnostics(self, include_volumes=None):
         uri = u"/api/Diagnostic"
         params = {u"incVolumes": include_volumes}
-        return self._default_session.get(uri, params=params)
+        return Py42Response(self._default_session.get(uri, params=params), "data")
 
     def get_alert_log(self, status=None, alert_type=None, page_num=None, page_size=None):
         uri = u"/api/AlertLog"
         params = {u"status": status, u"type": alert_type, u"pgNum": page_num, u"pgSize": page_size}
-        return self._default_session.get(uri, params=params)
+        return Py42Response(self._default_session.get(uri, params=params), "data")
 
     def get_current_tenant(self):
         uri = u"/c42api/v3/customer/my"
-        return self._default_session.get(uri)
+        return Py42Response(self._default_session.get(uri), "data")

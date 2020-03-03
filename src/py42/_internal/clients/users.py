dbbd1329d8bf9296809a8e68f616c660cd51c5af
Tim Abramson <tim.abramson@code42.com>
2020-03-03 13:18:56 -0600

    updated UserClient getter methods to accept optional params

--- src/py42/_internal/clients/users.py
+++ src/py42/_internal/clients/users.py
@@ -16,34 +16,37 @@ class UserClient(BaseAuthorityClient):
         last_name=None,
         notes=None,
     ):
-        uri = u"/api/User"
+        uri = "/api/User"
         data = {
-            u"orgUid": org_uid,
-            u"username": username,
-            u"email": email,
-            u"password": password,
-            u"firstName": first_name,
-            u"lastName": last_name,
-            u"notes": notes,
+            "orgUid": org_uid,
+            "username": username,
+            "email": email,
+            "password": password,
+            "firstName": first_name,
+            "lastName": last_name,
+            "notes": notes,
         }
         return self._default_session.post(uri, data=json.dumps(data))
 
-    def get_by_id(self, user_id):
-        uri = u"/api/User/{0}".format(user_id)
-        return self._default_session.get(uri)
-
-    def get_by_uid(self, user_uid):
-        uri = u"/api/User/{0}?idType=uid".format(user_uid)
-        return self._default_session.get(uri)
-
-    def get_by_username(self, username):
-        uri = u"/api/User"
-        params = {u"username": username}
+    def get_by_id(self, user_id, **kwargs):
+        uri = "/api/User/{0}".format(user_id)
+        params = kwargs
         return self._default_session.get(uri, params=params)
 
-    def get_current(self):
-        uri = u"/api/User/my"
-        return self._default_session.get(uri)
+    def get_by_uid(self, user_uid, **kwargs):
+        uri = "/api/User/{0}".format(user_uid)
+        params = dict(idType="uid", **kwargs)
+        return self._default_session.get(uri, params=params)
+
+    def get_by_username(self, username, **kwargs):
+        uri = "/api/User"
+        params = dict(username=username, **kwargs)
+        return self._default_session.get(uri, params=params)
+
+    def get_current(self, **kwargs):
+        uri = "/api/User/my"
+        params = kwargs
+        return self._default_session.get(uri, params=params)
 
     def _get_page(
         self,
@@ -54,51 +57,54 @@ class UserClient(BaseAuthorityClient):
         page_num=None,
         page_size=None,
         q=None,
+        **kwargs,
     ):
-        uri = u"/api/User"
-        params = {
-            u"active": active,
-            u"email": email,
-            u"orgUid": org_uid,
-            u"roleId": role_id,
-            u"pgNum": page_num,
-            u"pgSize": page_size,
-            u"q": q,
-        }
+        uri = "/api/User"
+        params = dict(
+            active=active,
+            email=email,
+            orgUid=org_uid,
+            roleId=role_id,
+            pgNum=page_num,
+            pgSize=page_size,
+            q=q,
+            **kwargs,
+        )
 
         return self._default_session.get(uri, params=params)
 
-    def get_all(self, active=None, email=None, org_uid=None, role_id=None, q=None):
+    def get_all(self, active=None, email=None, org_uid=None, role_id=None, q=None, **kwargs):
         return get_all_pages(
             self._get_page,
             settings.items_per_page,
-            u"users",
+            "users",
             active=active,
             email=email,
             org_uid=org_uid,
             role_id=role_id,
             q=q,
+            **kwargs,
         )
 
     def block(self, user_id):
-        uri = u"/api/UserBlock/{0}".format(user_id)
+        uri = "/api/UserBlock/{0}".format(user_id)
         return self._default_session.put(uri)
 
     def unblock(self, user_id):
-        uri = u"/api/UserBlock/{0}".format(user_id)
+        uri = "/api/UserBlock/{0}".format(user_id)
         return self._default_session.delete(uri)
 
     def deactivate(self, user_id, block_user=None):
-        uri = u"/api/UserDeactivation/{0}".format(user_id)
-        data = {u"blockUser": block_user}
+        uri = "/api/UserDeactivation/{0}".format(user_id)
+        data = {"blockUser": block_user}
         return self._default_session.put(uri, data=json.dumps(data))
 
     def reactivate(self, user_id, unblock_user=None):
-        uri = u"/api/UserDeactivation/{0}".format(user_id)
-        params = {u"unblockUser": unblock_user}
+        uri = "/api/UserDeactivation/{0}".format(user_id)
+        params = {"unblockUser": unblock_user}
         return self._default_session.delete(uri, params=params)
 
     def change_org_assignment(self, user_id, org_id):
-        uri = u"/api/UserMoveProcess"
-        data = {u"userId": user_id, u"parentOrgId": org_id}
+        uri = "/api/UserMoveProcess"
+        data = {"userId": user_id, "parentOrgId": org_id}
         return self._default_session.post(uri, data=json.dumps(data))

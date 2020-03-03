daea5e3dee16c2bd4745f814977bd8b7e4c434c6
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-03 16:33:51 +0530

    INTEG-912 Changed return type to Py42Response for OrgClient methods

--- src/py42/_internal/clients/orgs.py
+++ src/py42/_internal/clients/orgs.py
@@ -2,6 +2,7 @@ import json
 
 from py42._internal.base_classes import BaseAuthorityClient
 from py42._internal.clients.util import get_all_pages
+from py42._internal.response import Py42Response
 import py42.settings as settings
 
 
@@ -17,41 +18,41 @@ class OrgClient(BaseAuthorityClient):
             u"parentOrgUid": parent_org_uid,
             u"classification": classification,
         }
-        return self._default_session.post(uri, data=json.dumps(data))
+        return Py42Response(self._default_session.post(uri, data=json.dumps(data)), "data")
 
     def get_by_id(self, org_id):
         uri = u"/api/Org/{0}".format(org_id)
-        return self._default_session.get(uri)
+        return Py42Response(self._default_session.get(uri), "data")
 
     def get_by_uid(self, org_uid):
         uri = u"/api/Org/{0}?idType=orgUid".format(org_uid)
-        return self._default_session.get(uri)
+        return Py42Response(self._default_session.get(uri), "data")
 
     def _get_orgs_page(self, page_num=None, page_size=None):
         uri = u"/api/Org"
         params = {u"pgNum": page_num, u"pgSize": page_size}
 
-        return self._default_session.get(uri, params=params)
+        return Py42Response(self._default_session.get(uri, params=params), "data")
 
     def get_all(self):
         return get_all_pages(self._get_orgs_page, settings.items_per_page, u"orgs")
 
     def block(self, org_id):
         uri = u"/api/OrgBlock/{0}".format(org_id)
-        return self._default_session.put(uri)
+        return Py42Response(self._default_session.put(uri), "")
 
     def unblock(self, org_id):
         uri = u"/api/OrgBlock/{0}".format(org_id)
-        return self._default_session.delete(uri)
+        return Py42Response(self._default_session.delete(uri), "")
 
     def deactivate(self, org_id):
         uri = u"/api/OrgDeactivation/{0}".format(org_id)
-        return self._default_session.put(uri)
+        return Py42Response(self._default_session.put(uri), "")
 
     def reactivate(self, org_id):
         uri = u"/api/OrgDeactivation/{0}".format(org_id)
-        return self._default_session.delete(uri)
+        return Py42Response(self._default_session.delete(uri), "")
 
     def get_current(self):
         uri = u"/api/Org/my"
-        return self._default_session.get(uri)
+        return Py42Response(self._default_session.get(uri), "data")

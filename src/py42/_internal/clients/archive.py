a4697db3fc93c0ea8589e889baa0016b22a8e427
Tim Abramson <tim.abramson@code42.com>
2020-03-04 09:24:17 -0600

    allow additional parameters on get_restore_history method

--- src/py42/_internal/clients/archive.py
+++ src/py42/_internal/clients/archive.py
@@ -13,9 +13,9 @@ class ArchiveClient(BaseAuthorityClient):
         uri = u"/c42api/v3/BackupSets/{}/{}".format(device_guid, destination_guid)
         return self._default_session.get(uri)
 
-    def get_restore_history(self, days, org_id=None, page_num=None, page_size=None):
+    def get_restore_history(self, days, org_id=None, page_num=None, page_size=None, **kwargs):
         uri = u"/api/RestoreHistory"
-        params = {u"days": days, u"orgId": org_id, u"pgNum": page_num, u"pgSize": page_size}
+        params = dict(days=days, orgId=org_id, pgNum=page_num, pgSize=page_size, **kwargs)
         return self._default_session.get(uri, params=params)
 
     def get_web_restore_info(self, src_guid, dest_guid):

7595d7b18bfc46cb641296a2c0c182d11c11921e
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-04 17:14:52 +0530

    INTEG-882 Fix changes as per Py42Response structure

--- src/py42/_internal/clients/util.py
+++ src/py42/_internal/clients/util.py
@@ -8,7 +8,7 @@ def get_all_pages(func, page_size, key, *args, **kwargs):
         page_num += 1
         response = func(*args, page_num=page_num, page_size=page_size, **kwargs)
         yield response
-        response_dict = json.loads(response.text)
+        response_dict = json.loads(response.api_response.text)
         response_dict = response_dict.get(u"data") if response_dict.get(u"data") else response_dict
         page_items = response_dict[key]
         item_count = len(page_items)
--- src/py42/_internal/response.py
+++ src/py42/_internal/response.py
@@ -6,7 +6,7 @@ class Py42Response(object):
     def __init__(self, requests_response, json_key=None):
         self._response = requests_response
         self._json_key = json_key
-        response_dict = json.loads(self._response.text)
+        response_dict = json.loads(self.api_response.text)
         self._data_root = response_dict.get("data") or response_dict
 
     def __getitem__(self, key):

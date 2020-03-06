8bf6dd0643d3e5fb15b3d6a1b56f43a6c613c12c
Juliya Smith <juliya.smith@code42.com>
2020-03-06 15:30:43 +0000

    Fix alert tests

--- tests/_internal/clients/test_alerts.py
+++ tests/_internal/clients/test_alerts.py
@@ -1,3 +1,4 @@
+import pytest
 import json
 
 from .conftest import TENANT_ID_FROM_RESPONSE
@@ -6,11 +7,14 @@ from py42.sdk.alert_query import AlertState, AlertQuery
 
 
 class TestAlertClient(object):
-    def test_search_posts_expected_data(self, mock_session, user_context, successful_response):
+    @pytest.fixture
+    def successful_post(self, mock_session, successful_response):
+        mock_session.post.return_value = successful_response
+
+    def test_search_posts_expected_data(self, mock_session, user_context, successful_post):
         alert_client = AlertClient(mock_session, user_context)
         _filter = AlertState.eq("OPEN")
         query = AlertQuery(TENANT_ID_FROM_RESPONSE, _filter)
-        mock_session.post.return_value = successful_response
         alert_client.search(query)
         post_data = json.loads(mock_session.post.call_args[1]["data"])
         assert (
@@ -26,8 +30,7 @@ class TestAlertClient(object):
             and post_data["groups"][0]["filters"][0]["value"] == "OPEN"
         )
 
-    def test_search_posts_to_expected_url(self, mock_session, user_context, successful_response):
-        mock_session.post.return_value = successful_response
+    def test_search_posts_to_expected_url(self, mock_session, user_context, successful_post):
         alert_client = AlertClient(mock_session, user_context)
         _filter = AlertState.eq("OPEN")
         query = AlertQuery(TENANT_ID_FROM_RESPONSE, _filter)
@@ -49,9 +52,8 @@ class TestAlertClient(object):
         )
 
     def test_get_query_details_when_given_single_alert_id_posts_expected_data(
-        self, mock_session, user_context, successful_response
+        self, mock_session, user_context, successful_post
     ):
-        mock_session.post.return_value = successful_response
         alert_client = AlertClient(mock_session, user_context)
         alert_client.get_query_details("ALERT_ID_1")
         post_data = json.loads(mock_session.post.call_args[1]["data"])
@@ -61,9 +63,8 @@ class TestAlertClient(object):
         )
 
     def test_get_query_details_when_given_tenant_id_posts_expected_data(
-        self, mock_session, user_context, successful_response
+        self, mock_session, user_context, successful_post
     ):
-        mock_session.post.return_value = successful_response
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.get_query_details(alert_ids, "some-tenant-id")
@@ -75,15 +76,16 @@ class TestAlertClient(object):
         )
 
     def test_get_query_details_posts_to_expected_url(
-        self, mock_session, user_context, successful_response
+        self, mock_session, user_context, successful_post
     ):
-        mock_session.post.return_value = successful_response
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.get_query_details(alert_ids)
         assert mock_session.post.call_args[0][0] == "/svc/api/v1/query-details"
 
-    def test_resolve_when_not_given_tenant_id_posts_expected_data(self, mock_session, user_context):
+    def test_resolve_when_not_given_tenant_id_posts_expected_data(
+        self, mock_session, user_context, successful_post
+    ):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.resolve(alert_ids)
@@ -95,7 +97,7 @@ class TestAlertClient(object):
         )
 
     def test_resolve_when_given_single_alert_id_posts_expected_data(
-        self, mock_session, user_context
+        self, mock_session, user_context, successful_post
     ):
         alert_client = AlertClient(mock_session, user_context)
         alert_client.resolve("ALERT_ID_1")
@@ -105,7 +107,9 @@ class TestAlertClient(object):
             and post_data["alertIds"][0] == "ALERT_ID_1"
         )
 
-    def test_resolve_when_given_tenant_id_posts_expected_data(self, mock_session, user_context):
+    def test_resolve_when_given_tenant_id_posts_expected_data(
+        self, mock_session, user_context, successful_post
+    ):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.resolve(alert_ids, "some-tenant-id")
@@ -116,13 +120,15 @@ class TestAlertClient(object):
             and post_data["alertIds"][1] == "ALERT_ID_2"
         )
 
-    def test_resolve_posts_to_expected_url(self, mock_session, user_context):
+    def test_resolve_posts_to_expected_url(self, mock_session, user_context, successful_post):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.resolve(alert_ids, "some-tenant-id")
         assert mock_session.post.call_args[0][0] == "/svc/api/v1/resolve-alert"
 
-    def test_reopen_when_not_given_tenant_id_posts_expected_data(self, mock_session, user_context):
+    def test_reopen_when_not_given_tenant_id_posts_expected_data(
+        self, mock_session, user_context, successful_post
+    ):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.reopen(alert_ids)
@@ -134,7 +140,7 @@ class TestAlertClient(object):
         )
 
     def test_reopen_when_given_single_alert_id_posts_expected_data(
-        self, mock_session, user_context
+        self, mock_session, user_context, successful_post
     ):
         alert_client = AlertClient(mock_session, user_context)
         alert_client.reopen("ALERT_ID_1")
@@ -144,7 +150,9 @@ class TestAlertClient(object):
             and post_data["alertIds"][0] == "ALERT_ID_1"
         )
 
-    def test_reopen_when_given_tenant_id_posts_expected_data(self, mock_session, user_context):
+    def test_reopen_when_given_tenant_id_posts_expected_data(
+        self, mock_session, user_context, successful_post
+    ):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.reopen(alert_ids, "some-tenant-id")
@@ -155,7 +163,7 @@ class TestAlertClient(object):
             and post_data["alertIds"][1] == "ALERT_ID_2"
         )
 
-    def test_reopen_posts_to_expected_url(self, mock_session, user_context):
+    def test_reopen_posts_to_expected_url(self, mock_session, user_context, successful_post):
         alert_client = AlertClient(mock_session, user_context)
         alert_ids = ["ALERT_ID_1", "ALERT_ID_2"]
         alert_client.reopen(alert_ids, "some-tenant-id")

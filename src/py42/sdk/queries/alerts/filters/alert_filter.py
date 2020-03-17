82e74df67fdc13eef24d1e442b1bc56b5610b32d
Juliya Smith <juliya.smith@code42.com>
2020-03-17 17:22:21 -0500

    Fix where alerts are (#45)

--- src/py42/sdk/queries/alerts/filters/alert_filter.py
+++ src/py42/sdk/queries/alerts/filters/alert_filter.py
@@ -2,6 +2,7 @@ from py42.sdk.queries.query_filter import (
     QueryFilterStringField,
     create_filter_group,
     create_query_filter,
+    QueryFilterTimestampField,
 )
 
 
@@ -23,3 +24,34 @@ class AlertQueryFilterStringField(QueryFilterStringField):
     @classmethod
     def not_contains(cls, value):
         return create_not_contains_filter_group(cls._term, value)
+
+
+class DateObserved(QueryFilterTimestampField):
+    _term = u"CreatedAt"
+
+
+class Actor(AlertQueryFilterStringField):
+    _term = u"actor"
+
+
+class RuleName(AlertQueryFilterStringField):
+    _term = u"name"
+
+
+class Description(AlertQueryFilterStringField):
+    _term = u"description"
+
+
+class Severity(QueryFilterStringField):
+    _term = u"severity"
+
+    HIGH = u"HIGH"
+    MEDIUM = u"MEDIUM"
+    LOW = u"LOW"
+
+
+class AlertState(QueryFilterStringField):
+    _term = u"state"
+
+    OPEN = u"OPEN"
+    DISMISSED = u"RESOLVED"

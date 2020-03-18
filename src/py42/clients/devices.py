b74edb6d3b7f6d536443d45cdc58415b1edf3e39
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-18 18:47:23 +0530

    Added documentation for DeviceClient

--- src/py42/clients/devices.py
+++ src/py42/clients/devices.py
@@ -5,6 +5,10 @@ from py42.clients.util import get_all_pages
 
 
 class DeviceClient(BaseClient):
+    """
+        Class to represent a storage device/node/machine/computer
+    """
+
     def _get_page(
         self,
         active=None,
@@ -46,20 +50,21 @@ class DeviceClient(BaseClient):
         q=None,
         **kwargs
     ):
-        """[summary]
-        
+        """Get all device information
+
         Args:
-            active ([type], optional): [description]. Defaults to None.
-            blocked ([type], optional): [description]. Defaults to None.
-            org_uid ([type], optional): [description]. Defaults to None.
-            user_uid ([type], optional): [description]. Defaults to None.
-            destination_guid ([type], optional): [description]. Defaults to None.
-            include_backup_usage ([type], optional): [description]. Defaults to None.
+            active ([bool], optional): device state, True or False. Defaults to None.
+            blocked ([bool], optional): blocked status, True or False. Defaults to None.
+            org_uid ([int], optional): Id of Organization. Defaults to None.
+            user_uid ([int], optional): Id of User. Defaults to None.
+            destination_guid ([int], optional): description. Defaults to None.
+            include_backup_usage ([type], bool): Flag to denote whether to include backup summary.
+             Defaults to None.
             include_counts (bool, optional): [description]. Defaults to True.
             q ([type], optional): [description]. Defaults to None.
-        
+
         Returns:
-            [type]: [description]
+            (Py42.sdk.response.Py42Response): A generator containing collection of Py42Response
         """
 
         return get_all_pages(
@@ -77,104 +82,106 @@ class DeviceClient(BaseClient):
         )
 
     def get_by_id(self, device_id, include_backup_usage=None, **kwargs):
-        """[summary]
-        
+        """Get device information by device_id
+
         Args:
-            device_id ([type]): [description]
-            include_backup_usage ([type], optional): [description]. Defaults to None.
-        
+            device_id ([int]): device_id/'computerId' of the node
+            include_backup_usage ([bool], optional): Flag to denote whether to include
+              backup summary]. Defaults to None.
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing device information
         """
         uri = u"/api/Computer/{0}".format(device_id)
         params = dict(incBackupUsage=include_backup_usage, **kwargs)
         return self._session.get(uri, params=params)
 
     def get_by_guid(self, guid, include_backup_usage=None, **kwargs):
-        """[summary]
-        
+        """Get device information by guid
+
         Args:
-            guid ([type]): [description]
-            include_backup_usage ([type], optional): [description]. Defaults to None.
-        
+            guid ([str]): guid of the device
+            include_backup_usage ([bool], optional): [Flag to denote whether to include
+              backup summary]. Defaults to None.
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing device information
         """
         uri = u"/api/Computer/{0}".format(guid)
         params = dict(idType=u"guid", incBackupUsage=include_backup_usage, **kwargs)
         return self._session.get(uri, params=params)
 
     def block(self, device_id):
-        """[summary]
-        
+        """Block a device, set blocked_status of the device true
+
         Args:
-            device_id ([type]): [description]
-        
+            device_id ([int]): device_id/'computerId' of the node
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing state of the API call
         """
         uri = u"/api/ComputerBlock/{0}".format(device_id)
         return self._session.put(uri)
 
     def unblock(self, device_id):
-        """[summary]
-        
+        """Unblock a device, set blocked_status of the device false
+
         Args:
-            device_id ([type]): [description]
-        
+            device_id ([int]): device_id/'computerId' of the node
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing state of the API call
         """
         uri = u"/api/ComputerBlock/{0}".format(device_id)
         return self._session.delete(uri)
 
     def deactivate(self, device_id):
-        """[summary]
-        
+        """Deactivate a device, set 'active' status to false
+
         Args:
-            device_id ([type]): [description]
-        
+            device_id ([int]): device_id/'computerId' of the node
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing state of the API call
         """
         uri = u"/api/v4/computer-deactivation/update"
         data = {u"id": device_id}
         return self._session.post(uri, data=json.dumps(data))
 
     def reactivate(self, device_id):
-        """[summary]
-        
+        """Activate a device, set 'active' status to true
+
         Args:
-            device_id ([type]): [description]
-        
+            device_id ([int]): device_id/'computerId' of the node
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing state of the API call
         """
         uri = u"/api/v4/computer-deactivation/remove"
         data = {u"id": device_id}
         return self._session.post(uri, data=json.dumps(data))
 
     def deauthorize(self, device_id):
-        """[summary]
-        
+        """Deauthorize the device
+
         Args:
-            device_id ([type]): [description]
-        
+            device_id ([int]): device_id/'computerId' of the node
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing state of the API call
         """
         uri = u"/api/ComputerDeauthorization/{0}".format(device_id)
         return self._session.put(uri)
 
     def get_settings(self, guid, keys=None):
-        """[summary]
-        
+        """Settings of the device
+
         Args:
-            guid ([type]): [description]
+            guid ([str]): guid of the device
             keys ([type], optional): [description]. Defaults to None.
-        
+
         Returns:
-            [type]: [description]
+            Py42.sdk.response.Py42Response: Py42Response containing settings information
         """
         uri = u"/api/v4/device-setting/view"
         params = {u"guid": guid, u"keys": keys}

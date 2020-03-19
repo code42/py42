17891e2a85e467ec5d1909ff5a70e50db5091da0
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-19 22:47:30 +0530

    Removed unused Errors

--- src/py42/exceptions.py
+++ src/py42/exceptions.py
@@ -2,28 +2,6 @@ class Py42Error(Exception):
     """A generic, Py42-specific exception."""
 
 
-class Py42InitializationError(Py42Error):
-    def __init__(self, host_address, error):
-        self.host_address = host_address
-        self.error = str(error)
-        super(Py42InitializationError, self).__init__(self._message)
-
-    @property
-    def _message(self):
-        return (
-            u"Invalid credentials or host address ({0}). Check that the username and password are correct, that the "
-            u"host is available and reachable, and that you have supplied the full scheme, domain, and port "
-            u"(e.g. https://myhost.code42.com:4285). If you are using a self-signed ssl certificate, try setting "
-            u"py42.settings.verify_ssl_certs to false (or using a cert from a legitimate certificate "
-            u"authority). \n Failed with error {1} ".format(self.host_address, self.error)
-        )
-
-
-class Py42AuthenticationError(Py42Error):
-    def __init__(self, error_message):
-        super(Py42AuthenticationError, self).__init__(error_message)
-
-
 class Py42ArchiveFileNotFoundError(Py42Error):
     def __init__(self, device_guid, file_path):
         message = u"File not found in archive " u"for device {0} at path {1}".format(
@@ -39,6 +17,15 @@ class Py42FeatureUnavailableError(Py42Error):
         )
 
 
+class Py42SessionInitializationError(Py42Error):
+    def __init__(self, exception):
+        error_message = (
+            u"An error occurred while requesting "
+            u"server environment information, caused by {0}".format(str(exception))
+        )
+        super(Py42SessionInitializationError, self).__init__(error_message)
+
+
 class Py42SecurityPlanConnectionError(Py42Error):
     def __init__(self, error_message):
         super(Py42SecurityPlanConnectionError, self).__init__(error_message)
@@ -47,20 +34,6 @@ class Py42SecurityPlanConnectionError(Py42Error):
 class Py42StorageSessionInitializationError(Py42Error):
     def __init__(self, error_message):
         super(Py42StorageSessionInitializationError, self).__init__(error_message)
-
-
-class Py42DestinationNotFoundError(Py42Error):
-    def __init__(self, error_message):
-        super(Py42DestinationNotFoundError, self).__init__(error_message)
-
-
-class Py42SessionInitializationError(Py42Error):
-    def __init__(self, exception):
-        error_message = (
-            u"An error occurred while requesting "
-            u"server environment information, caused by {0}".format(str(exception))
-        )
-        super(Py42SessionInitializationError, self).__init__(error_message)
 
 
 class Py42BadRequestError(Py42Error):
@@ -89,7 +62,6 @@ class Py42InternalServerError(Py42Error):
 
 
 def raise_py42_error(raised_error):
-
     if raised_error.response.status_code == 400:
         raise Py42BadRequestError(raised_error)
     elif raised_error.response.status_code == 401:

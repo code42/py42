6e6d80b98ca62f58a610bce0f5f29efcbb3255df
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-03-19 22:20:47 +0530

    Removed unused methods

--- src/py42/_internal/auth_handling.py
+++ src/py42/_internal/auth_handling.py
@@ -16,14 +16,6 @@ class AuthHandler(object):
     def response_indicates_unauthorized(response):
         return response.status_code == 401
 
-    @staticmethod
-    def response_indicates_forbidden(response):
-        return response.status_code == 403
-
-    @staticmethod
-    def response_indicates_server_error(response):
-        return 500 <= response.response.status_code < 600
-
 
 class HeaderModifier(object):
     def __init__(self, header_name=u"Authorization", value_format=u"{0}"):

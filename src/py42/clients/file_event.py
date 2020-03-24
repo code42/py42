d6b9f18a21577fd3790e691fcf33bb1c7e49cb95
Tim Abramson <tim.abramson@code42.com>
2020-03-24 14:26:30 -0500

    a start

--- src/py42/clients/file_event.py
+++ src/py42/clients/file_event.py
@@ -3,7 +3,15 @@ from py42.clients import BaseClient
 
 
 class FileEventClient(BaseClient):
+    """A client for interacting with Code42 Forensic File Search events.
+    """
+
     def search(self, query):
+        """Searches for file events matching query criteria.
+
+        Args:
+            query ():
+        """
         query = str(query)
         uri = u"/forensic-search/queryservice/api/v1/fileevent"
         return self._session.post(uri, data=query)

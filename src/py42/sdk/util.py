75e0977c98a86cbc94a13a0304e9c43b54768c5d
Juliya Smith <juliya.smith@code42.com>
2020-03-25 15:38:22 +0000

    add returns turn types  + feedbacks

--- src/py42/sdk/util.py
+++ src/py42/sdk/util.py
@@ -8,7 +8,10 @@ def format_json(json_string):
     """Converts a minified JSON str to a prettified JSON str.
 
     Args:
-        json_string (str): A str representing JSON.
+        json_string (str): A str representing minified JSON.
+
+    Returns:
+        (str): A str representing prettified JSON.
     """
     parsed = json.loads(json_string)
     return json.dumps(parsed, indent=4)
@@ -31,20 +34,30 @@ def print_response(response, label=None):
 
 
 def convert_timestamp_to_str(timestamp):
-    """Converts the given POSIX timestamp to a date str.
+    """Converts the given POSIX timestamp to a date str. The format matches strftime
+    directives %Y-%m-%dT%H:%M:%S.%f.
 
     Args:
         timestamp (float): A POSIX timestamp.
+
+    Returns:
+        (str): A str representing the given timestamp. Example output looks like
+        '2020-03-25T15:29:04.465Z'.
     """
     date = datetime.utcfromtimestamp(timestamp)
     return convert_datetime_to_timestamp_str(date)
 
 
 def convert_datetime_to_timestamp_str(date):
-    """Converts the given datetime to a formatted date str.
+    """Converts the given datetime to a formatted date str. The format matches strftime
+    directives %Y-%m-%dT%H:%M:%S.%f.
 
     Args:
         date (datetime)
+
+    Returns:
+        (str): A str representing the given date. Example output looks like
+        '2020-03-25T15:29:04.465Z'.
     """
     prefix = date.strftime(u"%Y-%m-%dT%H:%M:%S.%f")[:-3]
     return u"{0}Z".format(prefix)

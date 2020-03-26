216c5bb8075b1e037da64ed90df8669476ecd8d7
Tim Abramson <tim.abramson@code42.com>
2020-03-26 11:10:04 -0500

    Doc File filters.

--- src/py42/sdk/queries/fileevents/filters/file_filter.py
+++ src/py42/sdk/queries/fileevents/filters/file_filter.py
@@ -3,24 +3,38 @@ from py42.sdk.queries.query_filter import QueryFilterStringField
 
 
 class FileCategory(QueryFilterStringField):
+    """Class that filters events by category of the file observed.
+    `Available File Categories <https://support.code42.com/Administrator/Cloud/Monitoring_and_managing/Forensic_File_Search_file_categories>`__
+    """
+
     _term = u"fileCategory"
 
 
 class FileName(FileEventFilterStringField):
+    """Class that filters events by the name of the file observed."""
+
     _term = u"fileName"
 
 
 class FileOwner(FileEventFilterStringField):
+    """Class that filters events by owner of the file observed."""
+
     _term = u"fileOwner"
 
 
 class FilePath(FileEventFilterStringField):
+    """Class that filters events by path of the file observed."""
+
     _term = u"filePath"
 
 
 class MD5(FileEventFilterStringField):
+    """Class that filters events by the MD5 hash of the file observed."""
+
     _term = u"md5Checksum"
 
 
 class SHA256(FileEventFilterStringField):
+    """Class that filters events by SHA256 hash of the file observed."""
+
     _term = u"sha256Checksum"

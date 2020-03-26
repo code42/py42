4bfa4b14bf2f34098881042ed4d6cb536bb077f9
Tim Abramson <tim.abramson@code42.com>
2020-03-26 11:10:40 -0500

    Doc Device filters.

--- src/py42/sdk/queries/fileevents/filters/device_filter.py
+++ src/py42/sdk/queries/fileevents/filters/device_filter.py
@@ -2,16 +2,24 @@ from py42.sdk.queries.fileevents.file_event_query import FileEventFilterStringFi
 
 
 class DeviceUsername(FileEventFilterStringField):
+    """Class that filters events by Code42 username of device that observed the event."""
+
     _term = u"deviceUserName"
 
 
 class OSHostname(FileEventFilterStringField):
+    """Class that filters events by Hostname of device that observed the event."""
+
     _term = u"osHostName"
 
 
 class PrivateIPAddress(FileEventFilterStringField):
+    """Class that filters events by private (lan) IP address of device that observed the event."""
+
     _term = u"privateIpAddresses"
 
 
 class PublicIPAddress(FileEventFilterStringField):
+    """Class that filters events by public (wan) IP address of device that observed the event."""
+
     _term = u"publicIpAddress"

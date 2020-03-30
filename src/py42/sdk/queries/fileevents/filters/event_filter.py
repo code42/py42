6db8e288d7e72f65e63ac4733c6a66639b27d125
Tim Abramson <tim.abramson@code42.com>
2020-03-30 09:14:17 -0500

    Doc event_filter.py

--- src/py42/sdk/queries/fileevents/filters/event_filter.py
+++ src/py42/sdk/queries/fileevents/filters/event_filter.py
@@ -3,10 +3,14 @@ from py42.sdk.queries.query_filter import QueryFilterTimestampField
 
 
 class EventTimestamp(QueryFilterTimestampField):
+    """Class that filters events based on the timestamp event occurred."""
+
     _term = u"eventTimestamp"
 
 
 class EventType(FileEventFilterStringField):
+    """Class that filters file events based on event type."""
+
     _term = u"eventType"
 
     CREATED = u"CREATED"
@@ -16,6 +20,10 @@ class EventType(FileEventFilterStringField):
 
 
 class InsertionTimestamp(QueryFilterTimestampField):
+    """Class that filters events based on the timestamp the event was actually added to the event
+    store (which can be after the event occurred on the device itself).
+    """
+
     _term = u"insertionTimestamp"
 
 

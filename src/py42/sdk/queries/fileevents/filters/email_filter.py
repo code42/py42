df9c0128f0e7d894e39544f8a58de3c381aecf71
Tim Abramson <tim.abramson@code42.com>
2020-03-30 08:41:07 -0500

    rephrase email filter docs.

--- src/py42/sdk/queries/fileevents/filters/email_filter.py
+++ src/py42/sdk/queries/fileevents/filters/email_filter.py
@@ -2,12 +2,12 @@ from py42.sdk.queries.fileevents.file_event_query import FileEventFilterStringFi
 
 
 class EmailRecipients(FileEventFilterStringField):
-    """Class that filters events based on the email recipients involved in the email exposure."""
+    """Class that filters events based on the email's recipient list (applies to email events only)."""
 
     _term = u"emailRecipients"
 
 
 class EmailSender(FileEventFilterStringField):
-    """Class that filters events based on the email sender involved in the email exposure."""
+    """Class that filters events based on the email's sender (applies to email events only)."""
 
     _term = u"emailSender"

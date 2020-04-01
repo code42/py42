dc5bc65a209413a14fe60786ed1a52464430c509
Tim Abramson <tim.abramson@code42.com>
2020-04-01 08:00:53 -0500

    - Added EmailPolicyName, EmailSubject, EmailFrom filter classes - Changed base class on Email filters to QueryFilterStringField to match options available in console (no "exists" or "not exists").

--- src/py42/sdk/queries/fileevents/filters/email_filter.py
+++ src/py42/sdk/queries/fileevents/filters/email_filter.py
@@ -1,13 +1,35 @@
-from py42.sdk.queries.fileevents.file_event_query import FileEventFilterStringField
+from py42.sdk.queries.query_filter import QueryFilterStringField
 
 
-class EmailRecipients(FileEventFilterStringField):
+class EmailPolicyName(QueryFilterStringField):
+    """Class that filters events based on the email DLP policy that detected this file (applies to
+    emails sent via Microsoft Office 365 only).
+    """
+
+    _term = u"emailDlpPolicyNames"
+
+
+class EmailSubject(QueryFilterStringField):
+    """Class that filters events based on the email's subject (applies to email events only)."""
+
+    _term = u"emailSubject"
+
+
+class EmailRecipients(QueryFilterStringField):
     """Class that filters events based on the email's recipient list (applies to email events only)."""
 
     _term = u"emailRecipients"
 
 
-class EmailSender(FileEventFilterStringField):
+class EmailSender(QueryFilterStringField):
     """Class that filters events based on the email's sender (applies to email events only)."""
 
     _term = u"emailSender"
+
+
+class EmailFrom(QueryFilterStringField):
+    """Class that filters events based on the display name of the email's sender, as it appears in
+    the \"From:\" field in the email (applies to email events only).
+    """
+
+    _term = u"emailFrom"

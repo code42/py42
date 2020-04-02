4b86758b06596443ad6b087281aadd562c34ce7e
Kiran Chaudhary <61223509+kiran-chaudhary@users.noreply.github.com>
2020-04-02 19:06:36 +0530

    Chore/docs user context (#55)

--- src/py42/sdk/usercontext.py
+++ src/py42/sdk/usercontext.py
@@ -1,9 +1,12 @@
 class UserContext(object):
+    """An object representing the currently logged in user."""
+
     def __init__(self, administration_client):
         self._administration_client = administration_client
         self._tenant_id = None
 
     def get_current_tenant_id(self):
+        """Gets the currently signed in user's tenant ID."""
         if self._tenant_id is None:
             self._tenant_id = self._get_tenant_id()
         return self._tenant_id

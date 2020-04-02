cc8188b813be5f9276516b630bfb8c32e280983c
Tim Abramson <tim.abramson@code42.com>
2020-04-02 13:13:55 -0500

    update InsertionTimestamp filter to give details on the timestamp argument requirement.

--- src/py42/sdk/queries/fileevents/filters/event_filter.py
+++ src/py42/sdk/queries/fileevents/filters/event_filter.py
@@ -38,7 +38,8 @@ class InsertionTimestamp(QueryFilterTimestampField):
     """Class that filters events based on the timestamp of when the event was actually added to the
     event store (which can be after the event occurred on the device itself).
 
-    `value` must be a POSIX timestamp. ( See [Basicsdates](basics.md) )
+    `value` must be a POSIX timestamp. (see the :ref:`Dates <anchor_dates>` section of the Basics
+    userguide for details on timestamp arguments in py42)
     """
 
     _term = u"insertionTimestamp"

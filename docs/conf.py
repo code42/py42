b0f07b350a849014369c1af99561bf52d205648d
Alan Grgic <alan.grgic@code42.com>
2020-04-09 11:11:30 -0500

    Chore/docs nav branding (#73)

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u"2020, Code42 Software"
 author = u"Code42 Software"
 
 # The short X.Y version
-version = meta.__version__
+version = "py42 v{}".format(meta.__version__)
 # The full version, including alpha/beta/rc tags
-release = meta.__version__
+release = "py42 v{}".format(meta.__version__)
 
 
 # -- General configuration ---------------------------------------------------
@@ -77,16 +77,22 @@ html_theme = "sphinx_rtd_theme"
 
 html_favicon = "favicon.ico"
 
+html_logo = "logo.png"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"style_nav_header_background": "#f0f0f0", "logo_only": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ["custom.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.

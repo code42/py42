e3a8a104fff98adc140b8b47732db525f89b66fa
Tim Abramson <tim.abramson@code42.com>
2020-04-21 13:18:38 -0500

    indentation

--- src/py42/clients/devices.py
+++ src/py42/clients/devices.py
@@ -101,7 +101,7 @@ class DeviceClient(BaseClient):
         Args:
             device_id (int): The identification number of the device.
             include_backup_usage (bool, optional): A flag to denote whether to include the
-            destination and its backup stats. Defaults to None.
+                destination and its backup stats. Defaults to None.
 
         Returns:
             :class:`py42.response.Py42Response`: A response containing device information.

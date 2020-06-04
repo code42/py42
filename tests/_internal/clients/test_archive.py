04057bc341caa5933468d2e08e825e25d126d73b
Alan Grgic <alan.grgic@code42.com>
2020-06-04 16:56:49 -0500

    finish tests

--- tests/_internal/clients/test_archive.py
+++ tests/_internal/clients/test_archive.py
@@ -13,10 +13,14 @@ MOCK_GET_ORG_RESTORE_HISTORY_RESPONSE = (
 
 MOCK_EMPTY_GET_ORG_RESTORE_HISTORY_RESPONSE = """{"totalCount": 3000, "restoreEvents": []}"""
 
+MOCK_GET_ORG_COLD_STORAGE_RESPONSE = """{"coldStorageRows": [{"archiveGuid": "fakeguid"}]}"""
+
+MOCK_EMPTY_GET_ORG_COLD_STORAGE_RESPONSE = """{"coldStorageRows": []}"""
+
 
 class TestArchiveClient(object):
     @pytest.fixture
-    def mock_get_all_response(self, mocker):
+    def mock_get_all_restore_history_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
         response.encoding = "utf-8"
@@ -24,22 +28,41 @@ class TestArchiveClient(object):
         return Py42Response(response)
 
     @pytest.fixture
-    def mock_get_all_empty_response(self, mocker):
+    def mock_get_all_restore_history_empty_response(self, mocker):
         response = mocker.MagicMock(spec=Response)
         response.status_code = 200
         response.encoding = "utf-8"
         response.text = MOCK_EMPTY_GET_ORG_RESTORE_HISTORY_RESPONSE
         return Py42Response(response)
 
+    @pytest.fixture
+    def mock_get_all_org_cold_storage_response(self, mocker):
+        response = mocker.MagicMock(spec=Response)
+        response.status_code = 200
+        response.encoding = "utf-8"
+        response.text = MOCK_GET_ORG_COLD_STORAGE_RESPONSE
+        return Py42Response(response)
+
+    @pytest.fixture
+    def mock_get_all_org_cold_storage_empty_response(self, mocker):
+        response = mocker.MagicMock(spec=Response)
+        response.status_code = 200
+        response.encoding = "utf-8"
+        response.text = MOCK_EMPTY_GET_ORG_COLD_STORAGE_RESPONSE
+        return Py42Response(response)
+
     def test_get_all_restore_history_calls_get_expected_number_of_times(
-        self, mock_session, mock_get_all_response, mock_get_all_empty_response
+        self,
+        mock_session,
+        mock_get_all_restore_history_response,
+        mock_get_all_restore_history_empty_response,
     ):
         py42.settings.items_per_page = 1
         client = ArchiveClient(mock_session)
         mock_session.get.side_effect = [
-            mock_get_all_response,
-            mock_get_all_response,
-            mock_get_all_empty_response,
+            mock_get_all_restore_history_response,
+            mock_get_all_restore_history_response,
+            mock_get_all_restore_history_empty_response,
         ]
         for _ in client.get_all_restore_history(10, "orgId", "123"):
             pass
@@ -58,6 +81,37 @@ class TestArchiveClient(object):
         )
 
     def test_get_all_org_cold_storage_archives_calls_get_expected_number_of_times(
-        self, mock_session
+        self,
+        mock_session,
+        mock_get_all_org_cold_storage_response,
+        mock_get_all_org_cold_storage_empty_response,
     ):
-        pass
+        py42.settings.items_per_page = 1
+        client = ArchiveClient(mock_session)
+        mock_session.get.side_effect = [
+            mock_get_all_org_cold_storage_response,
+            mock_get_all_org_cold_storage_response,
+            mock_get_all_org_cold_storage_empty_response,
+        ]
+        for _ in client.get_all_org_cold_storage_archives("orgId"):
+            pass
+        py42.settings.items_per_page = 1000
+        assert mock_session.get.call_count == 3
+
+    def test_get_all_org_cold_storage_archives_calls_get_with_expected_uri_and_params(
+        self, mock_session, mock_get_all_org_cold_storage_empty_response
+    ):
+        client = ArchiveClient(mock_session)
+        mock_session.get.side_effect = [mock_get_all_org_cold_storage_empty_response]
+        for _ in client.get_all_org_cold_storage_archives("orgId"):
+            break
+
+        params = {
+            "orgId": "orgId",
+            "incChildOrgs": True,
+            "pgNum": 1,
+            "pgSize": 1000,
+            "srtDir": "asc",
+            "srtKey": "archiveHoldExpireDate",
+        }
+        mock_session.get.assert_called_once_with("/api/ColdStorage", params=params)

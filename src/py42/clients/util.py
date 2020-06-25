c23ea928d4904eeab46dbfbb2ad816673f30bc15
Juliya Smith <juliya.smith@code42.com>
2020-06-25 17:35:09 +0000

    Restore get_all_pages

--- src/py42/clients/util.py
+++ src/py42/clients/util.py
@@ -3,11 +3,12 @@ import py42.settings as settings
 
 def get_all_pages(func, key, *args, **kwargs):
     page_num = 0
-    page_size = kwargs.get("page_size") or settings.items_per_page
-    item_count = page_size
-    while item_count >= page_size:
+    if kwargs.get("page_size") is None:
+        kwargs["page_size"] = settings.items_per_page
+    item_count = kwargs["page_size"]
+    while item_count >= kwargs["page_size"]:
         page_num += 1
-        response = func(*args, page_num=page_num, page_size=page_size, **kwargs)
+        response = func(*args, page_num=page_num, **kwargs)
         yield response
         page_items = response[key]
         item_count = len(page_items)

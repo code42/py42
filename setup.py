d8621da0a146b1dd979d12a7a1ab542d006cda33
Alan Grgic <alan.grgic@code42.com>
2020-07-16 14:54:41 -0500

    set IDE config files

--- setup.py
+++ setup.py
@@ -30,6 +30,11 @@ setup(
     zip_safe=False,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
     install_requires=["requests>=2.3"],
+    extras_require={
+        "dev": [
+            "tox>=3.17.1",
+        ]
+    },
     classifiers=[
         "Intended Audience :: Developers",
         "Natural Language :: English",

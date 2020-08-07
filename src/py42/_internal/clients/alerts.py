f3876994b15b77ca416b50aeb3551a2a8d8b08d5
Juliya Smith <juliya.smith@code42.com>
2020-08-07 18:25:34 +0000

    Make internal

--- src/py42/_internal/clients/alerts.py
+++ src/py42/_internal/clients/alerts.py
@@ -11,8 +11,8 @@ from py42.sdk.queries.query_filter import create_eq_filter_group
 class AlertClient(BaseClient):
     _uri_prefix = u"/svc/api/v1/{0}"
 
-    CREATED_AT = u"CreatedAt"
-    RULE_METADATA = u"ruleMetadata"
+    _CREATED_AT = u"CreatedAt"
+    _RULE_METADATA = u"ruleMetadata"
 
     def __init__(self, session, user_context):
         super(AlertClient, self).__init__(session)
@@ -76,32 +76,32 @@ class AlertClient(BaseClient):
         uri = self._uri_prefix.format(u"rules/query-rule-metadata")
         return self._session.post(uri, data=json.dumps(data))
 
-    def get_all_rules(self, sort_key=CREATED_AT, sort_direction=SortDirection.DESC):
+    def get_all_rules(self, sort_key=_CREATED_AT, sort_direction=SortDirection.DESC):
         return get_all_pages(
             self.get_rules_page,
-            self.RULE_METADATA,
+            self._RULE_METADATA,
             groups=None,
             sort_key=sort_key,
             sort_direction=sort_direction,
         )
 
     def get_all_rules_by_name(
-        self, rule_name, sort_key=CREATED_AT, sort_direction=SortDirection.DESC
+        self, rule_name, sort_key=_CREATED_AT, sort_direction=SortDirection.DESC
     ):
         return get_all_pages(
             self.get_rules_page,
-            self.RULE_METADATA,
+            self._RULE_METADATA,
             groups=[json.loads(str(create_eq_filter_group(u"Name", rule_name)))],
             sort_key=sort_key,
             sort_direction=sort_direction,
         )
 
     def get_rule_by_observer_id(
-        self, observer_id, sort_key=CREATED_AT, sort_direction=SortDirection.DESC
+        self, observer_id, sort_key=_CREATED_AT, sort_direction=SortDirection.DESC
     ):
         results = get_all_pages(
             self.get_rules_page,
-            self.RULE_METADATA,
+            self._RULE_METADATA,
             groups=[
                 json.loads(str(create_eq_filter_group(u"ObserverRuleId", observer_id)))
             ],

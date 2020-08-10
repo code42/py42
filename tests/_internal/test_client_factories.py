1575dd4687885858ef78b714832172c30e796127
Juliya Smith <juliya.smith@code42.com>
2020-08-10 14:25:25 +0000

    style fix

--- tests/_internal/test_client_factories.py
+++ tests/_internal/test_client_factories.py
@@ -13,15 +13,14 @@ from py42._internal.session_factory import SessionFactory
 from py42._internal.storage_session_manager import StorageSessionManager
 from py42._internal.token_providers import StorageTokenProviderFactory
 from py42.clients import administration
-from py42.clients import detectionlists
 from py42.clients import devices
 from py42.clients import file_event
 from py42.clients import legalhold
 from py42.clients import orgs
 from py42.clients import users
-from py42.exceptions import Py42FeatureUnavailableError
 from py42.clients.detectionlists.departing_employee import DepartingEmployeeClient
 from py42.clients.users import UserClient
+from py42.exceptions import Py42FeatureUnavailableError
 from py42.usercontext import UserContext
 
 TEST_ROOT_URL = "https://example.com"

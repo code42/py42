bcd35f96ff56270f7c218e9e1846a2947916de4f
Juliya Smith <juliya.smith@code42.com>
2020-08-20 14:51:14 +0000

    Put back u

--- src/py42/_internal/clients/archive.py
+++ src/py42/_internal/clients/archive.py
@@ -7,8 +7,8 @@ from py42.clients.util import get_all_pages
 
 class ArchiveClient(BaseClient):
     def get_data_key_token(self, device_guid):
-        uri = "/api/DataKeyToken"
-        data = {"computerGuid": device_guid}
+        uri = u"/api/DataKeyToken"
+        data = {u"computerGuid": device_guid}
         return self._session.post(uri, data=json.dumps(data))
 
     def get_single_archive(self, archive_guid):
@@ -21,7 +21,7 @@ class ArchiveClient(BaseClient):
         Returns:
             :class:`py42.response.Py42Response`: A response containing archive information.
         """
-        uri = "/api/Archive/{}".format(archive_guid)
+        uri = u"/api/Archive/{}".format(archive_guid)
         return self._session.get(uri)
 
     def get_page(self, page_num, page_size=None, **kwargs):
@@ -35,7 +35,7 @@ class ArchiveClient(BaseClient):
         Returns:
             :class:`py42.response.Py42Response`
         """
-        uri = "/api/Archive"
+        uri = u"/api/Archive"
         page_size = page_size or settings.items_per_page
         params = dict(pgNum=page_num, pgSize=page_size, **kwargs,)
         return self._session.get(uri, params=params)
@@ -47,23 +47,23 @@ class ArchiveClient(BaseClient):
         Args:
             id_value (int): Query value for archive.
             id_type (str): API query value description (e.g backupSourceGuid,
-                userUid, destiationGuid)
+                userUid, destinationGuid)
 
         Returns:
             generator: An object that iterates over :class:`py42.response.Py42Response` objects
             that each contain a page of archives.
         """
-        params = {"{}".format(id_type): "{}".format(id_value)}
-        return get_all_pages(self.get_page, "archives", **params,)
+        params = {u"{}".format(id_type): u"{}".format(id_value)}
+        return get_all_pages(self.get_page, u"archives", **params,)
 
     def get_backup_sets(self, device_guid, destination_guid):
-        uri = "/c42api/v3/BackupSets/{}/{}".format(device_guid, destination_guid)
+        uri = u"/c42api/v3/BackupSets/{}/{}".format(device_guid, destination_guid)
         return self._session.get(uri)
 
     def get_all_restore_history(self, days, id_type, id_value, **kwargs):
         return get_all_pages(
             self._get_restore_history_page,
-            "restoreEvents",
+            u"restoreEvents",
             days=days,
             id_type=id_type,
             id_value=id_value,
@@ -73,33 +73,33 @@ class ArchiveClient(BaseClient):
     def _get_restore_history_page(
         self, days, id_type, id_value, page_num, page_size, **kwargs
     ):
-        uri = "/api/RestoreHistory"
+        uri = u"/api/RestoreHistory"
         params = dict(days=days, pgNum=page_num, pgSize=page_size, **kwargs)
         params[id_type] = id_value
         return self._session.get(uri, params=params)
 
     def get_web_restore_info(self, src_guid, dest_guid):
-        uri = "/api/WebRestoreInfo"
-        params = {"srcGuid": src_guid, "destGuid": dest_guid}
+        uri = u"/api/WebRestoreInfo"
+        params = {u"srcGuid": src_guid, u"destGuid": dest_guid}
         return self._session.get(uri, params=params)
 
     def _get_cold_storage_archives_page(
         self,
         org_id=None,
         include_child_orgs=None,
-        sort_key="archiveHoldExpireDate",
-        sort_dir="asc",
+        sort_key=u"archiveHoldExpireDate",
+        sort_dir=u"asc",
         page_size=None,
         page_num=None,
     ):
-        uri = "/api/ColdStorage"
+        uri = u"/api/ColdStorage"
         params = {
-            "orgId": org_id,
-            "incChildOrgs": include_child_orgs,
-            "srtKey": sort_key,
-            "srtDir": sort_dir,
-            "pgSize": page_size,
-            "pgNum": page_num,
+            u"orgId": org_id,
+            u"incChildOrgs": include_child_orgs,
+            u"srtKey": sort_key,
+            u"srtDir": sort_dir,
+            u"pgSize": page_size,
+            u"pgNum": page_num,
         }
 
         return self._session.get(uri, params=params)
@@ -108,12 +108,12 @@ class ArchiveClient(BaseClient):
         self,
         org_id,
         include_child_orgs=True,
-        sort_key="archiveHoldExpireDate",
-        sort_dir="asc",
+        sort_key=u"archiveHoldExpireDate",
+        sort_dir=u"asc",
     ):
         return get_all_pages(
             self._get_cold_storage_archives_page,
-            "coldStorageRows",
+            u"coldStorageRows",
             org_id=org_id,
             include_child_orgs=include_child_orgs,
             sort_key=sort_key,
@@ -121,7 +121,7 @@ class ArchiveClient(BaseClient):
         )
 
     def update_cold_storage_purge_date(self, archive_guid, purge_date):
-        uri = "/api/coldStorage/{}".format(archive_guid)
-        params = {"idType": "guid"}
-        data = {"archiveHoldExpireDate": purge_date}
+        uri = u"/api/coldStorage/{}".format(archive_guid)
+        params = {u"idType": u"guid"}
+        data = {u"archiveHoldExpireDate": purge_date}
         return self._session.put(uri, params=params, data=json.dumps(data))

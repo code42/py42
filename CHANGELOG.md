d7434bf9d073041ed269da558e4e731f04b34a96
Juliya Smith <juliya.smith@code42.com>
2020-08-28 10:52:51 -0500

    Fix/within-the-last-silo (#183)
    * Generisize doc and make with_the_last only for file events
    
    * Move tests to correct location
    
    * Add float type
    
    * Add tests
    
    * CL
    
    * InsertionTimestamp within_the_last works
    
    * rm unneeded unicode header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 The intended audience of this file is for py42 consumers -- as such, changes that don't affect
 how a consumer would use the library (e.g. adding unit tests, updating documentation, etc) are not captured here.
 
+## Unreleased
+
+### Removed
+
+- Removed faulty `within_the_last()` method from `sdk.queries.alerts.filters.alert_filter.DateObserved`.
+
 ## 1.8.1 - 2020-08-28
 
 ### Fixed
--- src/py42/sdk/queries/query_filter.py
+++ src/py42/sdk/queries/query_filter.py
@@ -267,7 +267,7 @@ class QueryFilterTimestampField(object):
         provided ``value``.
 
         Args:
-            value (str or int): The value used to filter results.
+            value (str or int or float): The value used to filter results.
 
         Returns:
             :class:`~py42.sdk.queries.query_filter.FilterGroup`
@@ -282,7 +282,7 @@ class QueryFilterTimestampField(object):
         provided ``value``.
 
         Args:
-            value (str or int): The value used to filter results.
+            value (str or int or float): The value used to filter results.
 
         Returns:
             :class:`~py42.sdk.queries.query_filter.FilterGroup`
@@ -297,8 +297,8 @@ class QueryFilterTimestampField(object):
         the provided ``start_value`` and ``end_value``.
 
         Args:
-            start_value (str or int): The start value used to filter results.
-            end_value (str or int): The end value used to filter results.
+            start_value (str or int or float): The start value used to filter results.
+            end_value (str or int or float): The end value used to filter results.
 
         Returns:
             :class:`~py42.sdk.queries.query_filter.FilterGroup`
@@ -316,7 +316,7 @@ class QueryFilterTimestampField(object):
         calendar day as the provided ``value``.
 
         Args:
-            value (str or int): The value used to filter results.
+            value (str or int or float): The value used to filter results.
 
         Returns:
             :class:`~py42.sdk.queries.query_filter.FilterGroup`
@@ -333,20 +333,6 @@ class QueryFilterTimestampField(object):
         return create_in_range_filter_group(
             cls._term, formatted_start_time, formatted_end_time
         )
-
-    @classmethod
-    def within_the_last(cls, value):
-        """Returns a :class:`~py42.sdk.queries.query_filter.FilterGroup` that is useful
-        for finding results where the key ``self._term`` is an ``EventTimestamp._term``
-        and the value is one of the ``EventTimestamp`` attributes as ``value``.
-
-        Args:
-            value (str): `EventTimestamp` attribute.
-
-        Returns:
-            :class:`~py42.sdk.queries.query_filter.FilterGroup`
-        """
-        return create_within_the_last_filter_group(cls._term, value)
 
 
 class QueryFilterBooleanField(object):

051ae7ec097a29680b5632546eed958230621bc5
Disco <developer@onkraken.net>
2020-09-09 16:40:50 +0100

    Updated pg_num and pg_size parameters to page_number and page_size

--- src/py42/services/savedsearch.py
+++ src/py42/services/savedsearch.py
@@ -32,30 +32,32 @@ class SavedSearchService(BaseService):
         uri = u"{}/{}".format(self._resource, search_id)
         return self._connection.get(uri)
 
-    def get_query(self, search_id, pg_num=None, pg_size=None):
+    def get_query(self, search_id, page_number=None, page_size=None):
         """Get the saved search in form of a query(`py42.sdk.queries.fileevents.file_event_query`).
 
         Args:
             search_id (str): Unique search Id of the saved search.
-            pg_num (int, optional): The consecutive group of results of size pg_size in the result set to return. Defaults to 1.
-            pg_size (int, optional): The maximum number of results to be returned. Defaults to 10,000.
+            page_number (int, optional): The consecutive group of results of size pg_size in the result set to return. Defaults to 1.
+            page_size (int, optional): The maximum number of results to be returned. Defaults to 10,000.
         Returns:
             :class:`py42.sdk.queries.fileevents.file_event_query.FileEventQuery`
         """
         response = self.get_by_id(search_id)
         search = response[u"searches"][0]
-        return FileEventQuery.from_dict(search, page_number=pg_num, page_size=pg_size)
+        return FileEventQuery.from_dict(
+            search, page_number=page_number, page_size=page_size,
+        )
 
-    def execute(self, search_id, pg_num=None, pg_size=None):
+    def execute(self, search_id, page_number=None, page_size=None):
         """
         Execute a saved search for given search Id and return its results.
 
         Args:
             search_id (str): Unique search Id of the saved search.
-            pg_num (int, optional): The consecutive group of results of size pg_size in the result set to return. Defaults to 1.
-            pg_size (int, optional): The maximum number of results to be returned. Defaults to 10,000.
+            page_number (int, optional): The consecutive group of results of size pg_size in the result set to return. Defaults to 1.
+            page_size (int, optional): The maximum number of results to be returned. Defaults to 10,000.
         Returns:
             :class:`py42.response.Py42Response`
         """
-        query = self.get_query(search_id, pg_num=pg_num, pg_size=pg_size)
+        query = self.get_query(search_id, page_number=page_number, page_size=page_size)
         return self._file_event_client.search(query)

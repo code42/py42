3513cf04220eb9b6a602639f314fc56331a8b81a
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2020-09-14 10:30:52 -0500

    moved error handling into raise_py42_error and addressed compatibility for requests versions without json()

--- src/py42/services/_connection.py
+++ src/py42/services/_connection.py
@@ -13,7 +13,6 @@ from py42._compat import urljoin
 from py42._compat import urlparse
 from py42.exceptions import Py42Error
 from py42.exceptions import Py42FeatureUnavailableError
-from py42.exceptions import Py42MFARequiredError
 from py42.exceptions import raise_py42_error
 from py42.response import Py42Response
 from py42.services._auth import C42RenewableAuth
@@ -251,13 +250,7 @@ def _handle_error(method, url, response):
     try:
         response.raise_for_status()
     except HTTPError as ex:
-        if response.json()["error"][0]["primaryErrorKey"] in [
-            "TOTP_AUTH_CONFIGURATION_REQUIRED_FOR_USER",
-            "TIME_BASED_ONE_TIME_PASSWORD_REQUIRED",
-        ]:
-            raise Py42MFARequiredError(ex)
-        else:
-            raise_py42_error(ex)
+        raise_py42_error(ex)
 
 
 def _print_request(method, url, params=None, data=None):

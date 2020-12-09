94370adaee19fbb1380879b145acf329cc0612e9
Juliya Smith <juliya.smith@code42.com>
2020-12-09 10:51:18 -0600

    Fix/page token error (#241)

--- src/py42/services/fileevent.py
+++ src/py42/services/fileevent.py
@@ -1,4 +1,6 @@
 from py42._compat import str
+from py42.exceptions import Py42BadRequestError
+from py42.exceptions import Py42InvalidPageTokenError
 from py42.services import BaseService
 
 
@@ -21,9 +23,16 @@ class FileEventService(BaseService):
         Returns:
             :class:`py42.response.Py42Response`: A response containing the query results.
         """
-        query = str(query)
-        uri = u"/forensic-search/queryservice/api/v1/fileevent"
-        return self._connection.post(uri, data=query)
+        try:
+            query_str = str(query)
+            uri = u"/forensic-search/queryservice/api/v1/fileevent"
+            return self._connection.post(uri, data=query_str)
+        except Py42BadRequestError as err:
+            if u"INVALID_PAGE_TOKEN" in str(err.response.text):
+                page_token = query.page_token
+                if page_token:
+                    raise Py42InvalidPageTokenError(err, page_token)
+            raise
 
     def get_file_location_detail_by_sha256(self, checksum):
         """Get file location details based on SHA256 hash.
--- tests/services/test_file_event.py
+++ tests/services/test_file_event.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
 import pytest
+from requests import HTTPError
+from requests import Response
 
+from py42._compat import str
+from py42.exceptions import Py42BadRequestError
+from py42.exceptions import Py42InvalidPageTokenError
+from py42.sdk.queries.fileevents.file_event_query import FileEventQuery
+from py42.sdk.queries.fileevents.filters import FileName
 from py42.services._connection import Connection
 from py42.services.fileevent import FileEventService
 
-FILE_EVENT_URI = "/forensic-search/queryservice/api/v1/fileevent"
-RAW_QUERY = "RAW JSON QUERY"
-RAW_UNICODE_QUERY = u"RAW UNICODE JSON QUERY 我能吞"
+FILE_EVENT_URI = u"/forensic-search/queryservice/api/v1/fileevent"
+
+
+def _create_test_query(test_filename=u"*"):
+    return FileEventQuery(FileName.eq(test_filename))
+
+
+@pytest.fixture()
+def mock_invalid_page_token_connection(mocker, connection):
+    def side_effect(*args, **kwargs):
+        http_error = mocker.MagicMock(spec=HTTPError)
+        response = mocker.MagicMock(spec=Response)
+        response.text = "INVALID_PAGE_TOKEN"
+        http_error.response = response
+        raise Py42BadRequestError(http_error)
+
+    connection.post.side_effect = side_effect
+    return connection
 
 
 class TestFileEventService(object):
@@ -19,16 +41,56 @@ class TestFileEventService(object):
     ):
         service = FileEventService(connection)
         connection.post.return_value = successful_response
-        service.search(RAW_QUERY)
-        connection.post.assert_called_once_with(FILE_EVENT_URI, data=RAW_QUERY)
+        query = _create_test_query()
+        service.search(query)
+        connection.post.assert_called_once_with(FILE_EVENT_URI, data=str(query))
+
+    def test_search_when_given_page_token_and_bad_request_with_invalid_page_token_occurs_raises_invalid_page_token_error(
+        self, mock_invalid_page_token_connection
+    ):
+        query = _create_test_query()
+        query.page_token = "test_page_token"
+        service = FileEventService(mock_invalid_page_token_connection)
+        with pytest.raises(Py42InvalidPageTokenError) as err:
+            service.search(query)
+
+        assert str(err.value) == "Invalid page token: {}".format(query.page_token)
+
+    def test_search_when_bad_request_raised_and_token_not_in_query_raises_bad_request(
+        self, mock_invalid_page_token_connection
+    ):
+        query = _create_test_query()
+        query.page_token = None
+        service = FileEventService(mock_invalid_page_token_connection)
+        with pytest.raises(Py42BadRequestError):
+            service.search(query)
+
+    def test_search_when_bad_request_raised_with_token_but_has_not_invalid_token_text_raises_bad_request(
+        self, mocker, connection
+    ):
+        def side_effect(*args, **kwargs):
+            http_error = mocker.MagicMock(spec=HTTPError)
+            response = mocker.MagicMock(spec=Response)
+            response.text = "DIFFERENT_ERROR"
+            http_error.response = response
+            raise Py42BadRequestError(http_error)
+
+        connection.post.side_effect = side_effect
+        query = _create_test_query()
+        query.page_token = "test_page_token"
+        service = FileEventService(connection)
+        with pytest.raises(Py42BadRequestError):
+            service.search(query)
 
     def test_unicode_query_search_calls_post_with_query(
         self, connection, successful_response
     ):
         service = FileEventService(connection)
         connection.post.return_value = successful_response
-        service.search(RAW_UNICODE_QUERY)
-        connection.post.assert_called_once_with(FILE_EVENT_URI, data=RAW_UNICODE_QUERY)
+        query = _create_test_query(u"我能吞")
+        expected = str(query)
+        service.search(query)
+        connection.post.assert_called_once_with(FILE_EVENT_URI, data=expected)
 
     def test_get_file_location_detail_by_sha256_calls_get_with_hash(
         self, connection, successful_response

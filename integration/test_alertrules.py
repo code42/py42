6bf3a70b3d32a88f5b1ea02560ead4fcf8d0037c
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-12-15 17:19:03 +0530

    added tests

--- integration/test_alertrules.py
+++ integration/test_alertrules.py
@@ -1,9 +1,12 @@
 import pytest
 
 
-@pytest.mark.skip("Changes state of the system.")
-def test_rules_add_user():
-    pass
+@pytest.mark.skip("Failing with 404.")
+def test_rules_add_user(connection):
+    response = connection.alerts.rules.add_user(
+        "1cae9f92-5fd7-4504-b363-9bc45015adaa", 912249223544144039
+    )
+    assert response.status_code == 201
 
 
 def test_rules_get_all(connection):
@@ -19,11 +22,6 @@ def test_rules_get_by_observer_id(connection):
     assert response.status_code == 200
 
 
-@pytest.mark.skip("Changes state of the system.")
-def test_rules_remove_all_users():
-    pass
-
-
 def test_rules_get_all_by_name(connection):
     response_gen = connection.alerts.rules.get_all_by_name("Test Alerts using CLI")
     for response in response_gen:
@@ -36,9 +34,20 @@ def test_rules_get_page(connection):
     assert response.status_code == 200
 
 
-@pytest.mark.skip("Changes state of the system.")
-def test_rules_remove_user():
-    pass
+@pytest.mark.skip("Failing with 404.")
+def test_rules_remove_user(connection):
+    response = connection.alerts.rules.remove_user(
+        "1cae9f92-5fd7-4504-b363-9bc45015adaa", 912249223544144039
+    )
+    assert response.status_code == 204
+
+
+@pytest.mark.skip("Failing with 404.")
+def test_rules_remove_all_users(connection):
+    response = connection.alerts.rules.remove_all_users(
+        "1cae9f92-5fd7-4504-b363-9bc45015adaa"
+    )
+    assert response.status_code == 204
 
 
 def test_rules_exfiltration_get(connection):
@@ -47,7 +56,6 @@ def test_rules_exfiltration_get(connection):
     assert response.status_code == 200
 
 
-@pytest.mark.skip("Changes state of the system.")
 def test_rules_cloudshare_get(connection):
     rule_id = "1cae9f92-5fd7-4504-b363-9bc45015adaa"
     response = connection.alerts.rules.cloudshare.get(rule_id)

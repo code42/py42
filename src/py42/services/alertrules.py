e067fafced58b3e133c461aab6e20e6257a45851
Kiran Chaudhary <kiran.chaudhary@code42.com>
2020-12-23 17:09:35 +0530

    create user profile if not exists before adding user to a rule

--- src/py42/services/alertrules.py
+++ src/py42/services/alertrules.py
@@ -41,6 +41,7 @@ class AlertRulesService(BaseService):
 
     def add_user(self, rule_id, user_id):
         tenant_id = self._user_context.get_current_tenant_id()
+        self._user_profile_service.create_if_not_exists(user_id)
         user_details = self._user_profile_service.get_by_id(user_id)
         user_aliases = user_details.data.get(u"cloudUsernames") or []
         data = {

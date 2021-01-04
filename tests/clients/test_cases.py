8fce9254dc04f6a8fcb69b6a67dd863deb747d4f
Kiran Chaudhary <kiran.chaudhary@code42.com>
2021-01-04 19:51:07 +0530

    fix tests and style

--- tests/clients/test_cases.py
+++ tests/clients/test_cases.py
@@ -36,7 +36,7 @@ class TestCasesClient:
     ):
         cases_client = CasesClient(mock_cases_service, mock_cases_file_event_service)
         cases_client.get_all()
-        mock_cases_service.get_all.assert_called_once()
+        mock_cases_service.get_all.call_count == 1
 
     def test_get_case_by_case_number_calls_cases_service_with_expected_params(
         self, mock_cases_service, mock_cases_file_event_service
--- tests/integration/test_cases.py
+++ tests/integration/test_cases.py
@@ -3,39 +3,38 @@ import pytest
 
 @pytest.mark.integration
 class TestCases:
-
-    def test_create_case(connection):
+    def test_create_case(self, connection):
         response = connection.cases.create_case()
         assert response.status_code == 200
 
-    def test_get_all_cases(connection):
+    def test_get_all_cases(self, connection):
         response = connection.cases.get_all()
         assert response.status_code == 200
 
-    def test_get_case_by_case_number(connection):
+    def test_get_case_by_case_number(self, connection):
         response = connection.cases.get_case_by_case_number()
         assert response.status_code == 200
 
-    def test_update_case(connection):
+    def test_update_case(self, connection):
         response = connection.cases.update()
         assert response.status_code == 200
 
-    def test_export_summary(connection):
+    def test_export_summary(self, connection):
         response = connection.cases.export_summary()
         assert response.status_code == 200
 
-    def test_add_file_event(connection):
+    def test_add_file_event(self, connection):
         response = connection.cases.file_events.add_event()
         assert response.status_code == 200
 
-    def test_delete_file_event(connection):
+    def test_delete_file_event(self, connection):
         response = connection.cases.file_events.delete_event()
         assert response.status_code == 200
 
-    def test_get_file_event(connection):
+    def test_get_file_event(self, connection):
         response = connection.cases.file_events.get_event()
         assert response.status_code == 200
 
-    def test_get_all_file_events(connection):
+    def test_get_all_file_events(self, connection):
         response = connection.cases.file_events.get_all_events()
         assert response.status_code == 200

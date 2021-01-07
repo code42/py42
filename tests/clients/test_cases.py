abd7bc67d7aa57d00afeb7430e192d4f137b3478
Kiran Chaudhary <kiran.chaudhary@code42.com>
2021-01-07 14:57:26 +0530

    add more tests for date range validation

--- tests/clients/test_cases.py
+++ tests/clients/test_cases.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 
 from py42.clients.cases import CasesClient
@@ -99,6 +101,68 @@ class TestCasesClient:
             updated_at=updated_at_range,
             subject=None,
             assignee=None,
+            page_number=1,
+            page_size=100,
+            sort_direction="asc",
+            sort_key="number",
+        )
+
+    def test_get_all_converts_diff_types_to_ranges_and_calls_service_with_expected_params(
+        self, mock_cases_service, mock_cases_file_event_service
+    ):
+        cases_client = CasesClient(mock_cases_service, mock_cases_file_event_service)
+        cases_client.get_all(
+            created_at_begin_time=1609439400,
+            updated_at_begin_time="2021-02-01 00:00:00",
+            created_at_end_time=1612117800.0,
+            updated_at_end_time=datetime.strptime(
+                "2021-02-20 00:00:00", u"%Y-%m-%d %H:%M:%S"
+            ),
+            subject="subject",
+            assignee="a",
+            name="test",
+            status="closed",
+        )
+        created_at_range = "2020-12-31T18:30:00.000Z/2021-01-31T18:30:00.000Z"
+        updated_at_range = "2021-02-01T00:00:00.000Z/2021-02-20T00:00:00.000Z"
+
+        mock_cases_service.get_all.assert_called_once_with(
+            name="test",
+            status="closed",
+            created_at=created_at_range,
+            updated_at=updated_at_range,
+            subject="subject",
+            assignee="a",
+            page_number=1,
+            page_size=100,
+            sort_direction="asc",
+            sort_key="number",
+        )
+
+    def test_get_all_send_not_in_ranges_when_only_begin_or_end_time_is_specified(
+        self, mock_cases_service, mock_cases_file_event_service
+    ):
+        cases_client = CasesClient(mock_cases_service, mock_cases_file_event_service)
+        cases_client.get_all(
+            created_at_begin_time=1609439400,
+            updated_at_begin_time=None,
+            created_at_end_time=None,
+            updated_at_end_time=datetime.strptime(
+                "2021-02-20 00:00:00", u"%Y-%m-%d %H:%M:%S"
+            ),
+            subject="subject",
+            assignee="a",
+            name="test",
+            status="closed",
+        )
+
+        mock_cases_service.get_all.assert_called_once_with(
+            name="test",
+            status="closed",
+            created_at=None,
+            updated_at=None,
+            subject="subject",
+            assignee="a",
             page_number=1,
             page_size=100,
             sort_direction="asc",

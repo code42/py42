69c09531ae2886fdc98e9b5542f277c111124fd9
Juliya Smith <juliya.smith@code42.com>
2021-02-01 17:21:08 +0000

    u

--- src/py42/services/preservationdata.py
+++ src/py42/services/preservationdata.py
@@ -20,7 +20,7 @@ class PreservationDataService(BaseService):
         return self._connection.post(uri, json=data)
 
     def get_file_version_list(self, deviceGuid, file_md5, file_sha256, path):
-        params = "fileSHA256={}&fileMD5={}&deviceGuid={}&path={}"
+        params = u"fileSHA256={}&fileMD5={}&deviceGuid={}&path={}"
         params = params.format(file_sha256, file_md5, deviceGuid, quote(path))
         uri = u"/api/v1/FileVersionListing?{}".format(params)
         return self._connection.get(uri)

f61f2795fc2be8cee8ce21724325a384adeb3a17
Juliya Smith <juliya.smith@code42.com>
2021-04-13 12:04:42 +0000

    Simplify other isinstnace check

--- src/py42/util.py
+++ src/py42/util.py
@@ -115,7 +115,7 @@ def parse_timestamp_to_milliseconds_precision(timestamp):
 
 
 def parse_timestamp_to_microseconds_precision(timestamp):
-    if isinstance(timestamp, int) or isinstance(timestamp, float):
+    if isinstance(timestamp, (int, float)):
         return datetime.utcfromtimestamp(timestamp).strftime(_MICROSECOND_FORMAT)
     elif isinstance(timestamp, datetime):
         return timestamp.strftime(_MICROSECOND_FORMAT)

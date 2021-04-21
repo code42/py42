ccc99f33ead80e8201e8e79f4f5bce88a836299a
Juliya Smith <juliya.smith@code42.com>
2021-04-21 16:53:49 +0000

    missing space

--- src/py42/clients/alerts.py
+++ src/py42/clients/alerts.py
@@ -119,7 +119,7 @@ class AlertsClient(object):
         Args:
             alert_id (str): The identification number of an alert to add a note to.
             note (str): A note to attach to the alert. Must be less than 2000
-                characters.Defaults to None.
+                characters. Defaults to None.
 
         Returns:
             :class:`py42.response.Py42Response`

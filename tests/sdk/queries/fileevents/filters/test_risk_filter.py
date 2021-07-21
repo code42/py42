85d3f68f7252ed7c25e79febafcc696bf44d8dfe
Alan Grgic <alan.grgic@code42.com>
2021-07-21 16:33:09 -0500

    fmt

--- tests/sdk/queries/fileevents/filters/test_risk_filter.py
+++ tests/sdk/queries/fileevents/filters/test_risk_filter.py
@@ -3,7 +3,8 @@ from tests.sdk.queries.conftest import IS_IN
 from tests.sdk.queries.conftest import IS_NOT
 from tests.sdk.queries.conftest import NOT_IN
 
-from py42.sdk.queries.fileevents.filters.risk_filter import RiskIndicator, RiskSeverity
+from py42.sdk.queries.fileevents.filters.risk_filter import RiskIndicator
+from py42.sdk.queries.fileevents.filters.risk_filter import RiskSeverity
 
 
 def test_risk_indicator_eq_str_gives_correct_json_representation():
@@ -19,14 +20,22 @@ def test_risk_indicator_not_eq_str_gives_correct_json_representation():
 
 
 def test_risk_indicator_is_in_str_gives_correct_json_representation():
-    items = [RiskIndicator.FileCategories.EXECUTABLE, RiskIndicator.FileCategories.IMAGE, RiskIndicator.FileCategories.PDF]
+    items = [
+        RiskIndicator.FileCategories.EXECUTABLE,
+        RiskIndicator.FileCategories.IMAGE,
+        RiskIndicator.FileCategories.PDF,
+    ]
     _filter = RiskIndicator.is_in(items)
     expected = IS_IN.format("riskIndicatorNames", *items)
     assert str(_filter) == expected
 
 
 def test_risk_indicator_not_in_str_gives_correct_json_representation():
-    items = [RiskIndicator.FileCategories.EXECUTABLE, RiskIndicator.FileCategories.IMAGE, RiskIndicator.FileCategories.PDF]
+    items = [
+        RiskIndicator.FileCategories.EXECUTABLE,
+        RiskIndicator.FileCategories.IMAGE,
+        RiskIndicator.FileCategories.PDF,
+    ]
     _filter = RiskIndicator.not_in(items)
     expected = NOT_IN.format("riskIndicatorNames", *items)
     assert str(_filter) == expected

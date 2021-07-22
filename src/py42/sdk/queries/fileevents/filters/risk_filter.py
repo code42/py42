361e0348543648201cca346e61b0160852b66ed2
Alan Grgic <alan.grgic@code42.com>
2021-07-21 22:58:27 -0500

    docstrings

--- src/py42/sdk/queries/fileevents/filters/risk_filter.py
+++ src/py42/sdk/queries/fileevents/filters/risk_filter.py
@@ -3,7 +3,71 @@ from py42.util import get_attribute_keys_from_class
 
 
 class RiskIndicator(FileEventFilterStringField):
-    """Class that filters events by risk indicator."""
+    """Class that filters events by risk indicator.
+
+    Available options are provided as class attributes:
+        - :attr:`RiskIndicator.CloudDataExposures.PUBLIC_CORPORATE_BOX`
+        - :attr:`RiskIndicator.CloudDataExposures.PUBLIC_CORPORATE_GOOGLE_DRIVE`
+        - :attr:`RiskIndicator.CloudDataExposures.PUBLIC_CORPORATE_ONEDRIVE`
+        - :attr:`RiskIndicator.CloudDataExposures.SENT_CORPORATE_GMAIL`
+        - :attr:`RiskIndicator.CloudDataExposures.SHARED_CORPORATE_BOX`
+        - :attr:`RiskIndicator.CloudDataExposures.SHARED_CORPORATE_GOOGLE_DRIVE`
+        - :attr:`RiskIndicator.CloudDataExposures.SHARED_CORPORATE_ONEDRIVE`
+        - :attr:`RiskIndicator.CloudStorageUploads.AMAZON_DRIVE`
+        - :attr:`RiskIndicator.CloudStorageUploads.BOX`
+        - :attr:`RiskIndicator.CloudStorageUploads.DROPBOX`
+        - :attr:`RiskIndicator.CloudStorageUploads.GOOGLE_DRIVE`
+        - :attr:`RiskIndicator.CloudStorageUploads.ICLOUD`
+        - :attr:`RiskIndicator.CloudStorageUploads.MEGA`
+        - :attr:`RiskIndicator.CloudStorageUploads.ONEDRIVE`
+        - :attr:`RiskIndicator.CloudStorageUploads.ZOHO`
+        - :attr:`RiskIndicator.CodeRepositoryUploads.BITBUCKET`
+        - :attr:`RiskIndicator.CodeRepositoryUploads.GITHUB`
+        - :attr:`RiskIndicator.CodeRepositoryUploads.GITLAB`
+        - :attr:`RiskIndicator.CodeRepositoryUploads.SOURCEFORGE`
+        - :attr:`RiskIndicator.CodeRepositoryUploads.STASH`
+        - :attr:`RiskIndicator.EmailServiceUploads.ONESIXTHREE_DOT_COM`
+        - :attr:`RiskIndicator.EmailServiceUploads.ONETWOSIX_DOT_COM`
+        - :attr:`RiskIndicator.EmailServiceUploads.AOL`
+        - :attr:`RiskIndicator.EmailServiceUploads.COMCAST`
+        - :attr:`RiskIndicator.EmailServiceUploads.GMAIL`
+        - :attr:`RiskIndicator.EmailServiceUploads.ICLOUD`
+        - :attr:`RiskIndicator.EmailServiceUploads.MAIL_DOT_COM`
+        - :attr:`RiskIndicator.EmailServiceUploads.OUTLOOK`
+        - :attr:`RiskIndicator.EmailServiceUploads.PROTONMAIL`
+        - :attr:`RiskIndicator.EmailServiceUploads.QQMAIL`
+        - :attr:`RiskIndicator.EmailServiceUploads.SINA_MAIL`
+        - :attr:`RiskIndicator.EmailServiceUploads.SOHU_MAIl`
+        - :attr:`RiskIndicator.EmailServiceUploads.YAHOO`
+        - :attr:`RiskIndicator.EmailServiceUploads.ZOHO_MAIL`
+        - :attr:`RiskIndicator.RemovableMedia.AIRDROP`
+        - :attr:`RiskIndicator.RemovableMedia.REMOVABLE_MEDIA`
+        - :attr:`RiskIndicator.FileCategories.AUDIO`
+        - :attr:`RiskIndicator.FileCategories.DOCUMENT`
+        - :attr:`RiskIndicator.FileCategories.EXECUTABLE`
+        - :attr:`RiskIndicator.FileCategories.IMAGE`
+        - :attr:`RiskIndicator.FileCategories.PDF`
+        - :attr:`RiskIndicator.FileCategories.PRESENTATION`
+        - :attr:`RiskIndicator.FileCategories.SCRIPT`
+        - :attr:`RiskIndicator.FileCategories.SOURCE_CODE`
+        - :attr:`RiskIndicator.FileCategories.SPREADSHEET`
+        - :attr:`RiskIndicator.FileCategories.VIDEO`
+        - :attr:`RiskIndicator.FileCategories.VIRTUAL_DISK_IMAGE`
+        - :attr:`RiskIndicator.FileCategories.ZIP`
+        - :attr:`RiskIndicator.MessagingServiceUploads.FACEBOOK_MESSENGER`
+        - :attr:`RiskIndicator.MessagingServiceUploads.MICROSOFT_TEAMS`
+        - :attr:`RiskIndicator.MessagingServiceUploads.SLACK`
+        - :attr:`RiskIndicator.MessagingServiceUploads.WHATSAPP`
+        - :attr:`RiskIndicator.Other.OTHER`
+        - :attr:`RiskIndicator.MessagingServiceUploads.UNKNOWN`
+        - :attr:`RiskIndicator.SocialMediaUploads.FACEBOOK`
+        - :attr:`RiskIndicator.SocialMediaUploads.LINKEDIN`
+        - :attr:`RiskIndicator.SocialMediaUploads.REDDIT`
+        - :attr:`RiskIndicator.SocialMediaUploads.TWITTER`
+        - :attr:`RiskIndicator.UserBehavior.FILE_MISMATCH`
+        - :attr:`RiskIndicator.UserBehavior.OFF_HOURS`
+        - :attr:`RiskIndicator.UserBehavior.REMOTE`
+    """
 
     _term = u"riskIndicatorNames"
 
@@ -145,7 +209,15 @@ class RiskIndicator(FileEventFilterStringField):
 
 
 class RiskSeverity(FileEventFilterStringField):
-    """Class that filters events by risk severity."""
+    """Class that filters events by risk severity.
+
+    Available options are provided as class attributes:
+        - :attr:`RiskSeverity.LOW`
+        - :attr:`RiskSeverity.MODERATE`
+        - :attr:`RiskSeverity.HIGH`
+        - :attr:`RiskSeverity.CRITICAL`
+        - :attr:`RiskSeverity.NO_RISK_INDICATED`
+    """
 
     _term = u"riskSeverity"
 

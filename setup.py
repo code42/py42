a452965c81cd8d0f09a88f5b9260764528f18db6
Ryan Haley <ryan.haley@code42.com>
2021-07-29 17:01:56 -0500

    Correct python requires

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6 <4",
+    python_requires=">=3.6, <4",
     install_requires=["requests>=2.3"],
     extras_require={
         "dev": [

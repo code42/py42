2d9455fd7b3d7f0ce1485cffa9a883f9344b1466
Ryan Haley <ryan.haley@code42.com>
2021-07-29 16:47:44 -0500

    set python_requires 3.6 < x > 4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=3.6 <4",
     install_requires=["requests>=2.3"],
     extras_require={
         "dev": [

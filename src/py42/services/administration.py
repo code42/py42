0d339c49cdd8529d8e98a083ca8015d314f9869b
Tora Kozic <81983309+tora-kozic@users.noreply.github.com>
2021-10-15 15:37:07 -0500

    removing diagonistics api (#377)

--- src/py42/services/administration.py
+++ src/py42/services/administration.py
@@ -2,11 +2,6 @@ from py42.services import BaseService
 
 
 class AdministrationService(BaseService):
-    def get_diagnostics(self, include_volumes=None):
-        uri = "/api/Diagnostic"
-        params = {"incVolumes": include_volumes}
-        return self._connection.get(uri, params=params)
-
     def get_alert_log(
         self, status=None, alert_type=None, page_num=None, page_size=None
     ):
--- tests/integration/test_serveradmin.py
+++ tests/integration/test_serveradmin.py
@@ -11,7 +11,3 @@ class TestServerAdmin:
     def test_get_current_tenant(self, connection):
         response = connection.serveradmin.get_current_tenant()
         assert_successful_response(response)
-
-    def test_get_diagnostics(self, connection):
-        response = connection.serveradmin.get_diagnostics()
-        assert_successful_response(response)

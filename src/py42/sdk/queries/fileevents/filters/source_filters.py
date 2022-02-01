031580ccf04ca55995f436cbc3f048948ca1ed61
tim.abramson <tim.abramson@code42.com>
2022-02-01 13:32:59 -0600

    add SourceCategory choices

--- src/py42/sdk/queries/fileevents/filters/source_filters.py
+++ src/py42/sdk/queries/fileevents/filters/source_filters.py
@@ -1,10 +1,35 @@
-from py42.sdk.queries.fileevents.file_event_query import FileEventFilterStringField
+from py42.sdk.queries.fileevents.file_event_query import FileEventFilterStringField, Choices
 
 
-class SourceCategory(FileEventFilterStringField):
-    """Class that filters events by the source category."""
+class SourceCategory(FileEventFilterStringField, Choices):
+    """
+    Class that filters events by the source category.
+
+    Available options are provided as class attributes:
+        - :attr:`SourceCategory.BUSINESS_TOOLS`
+        - :attr:`SourceCategory.CLOUD_STORAGE`
+        - :attr:`SourceCategory.DEVICE`
+        - :attr:`SourceCategory.EMAIL`
+        - :attr:`SourceCategory.MESSAGING`
+        - :attr:`SourceCategory.MULTIPLE_POSSIBILITIES`
+        - :attr:`SourceCategory.SOCIAL_MEDIA`
+        - :attr:`SourceCategory.SOURCE_CODE_REPOSITORY`
+        - :attr:`SourceCategory.UNCATEGORIZED`
+        - :attr:`SourceCategory.UNKNOWN`
+        """
 
     _term = "sourceCategory"
+
+    BUSINESS_TOOLS = "Business Tools"
+    CLOUD_STORAGE = "Cloud Storage"
+    DEVICE = "Device"
+    EMAIL = "Email"
+    MESSAGING = "Messaging"
+    MULTIPLE_POSSIBILITIES = "Multiple Possibilities"
+    SOCIAL_MEDIA = "Social Media"
+    SOURCE_CODE_REPOSITORY = "Source Code Repository"
+    UNCATEGORIZED = "Uncategorized"
+    UNKNOWN = "Unknown"
 
 
 class SourceName(FileEventFilterStringField):

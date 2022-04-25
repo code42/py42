9242bcfa4f6bda160255b311789ce56f82cffc54
Tora Kozic <tora.kozic@code42.com>
2022-04-25 14:39:11 -0500

    add docs dependencies to setup

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = f"py42 v{meta.__version__}"
 
 # If your documentation needs a minimal Sphinx version, state it here.
 
-needs_sphinx = "4.0"
+needs_sphinx = "4.4.0"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom

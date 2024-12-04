3bc777d2b0bb008c560aa3ed2a0c0f43f6a85fde
Cecilia Stevens <63068179+ceciliastevens@users.noreply.github.com>
2024-12-04 12:51:51 -0500

    add user agent prefix and update default user agent

--- src/py42/settings/__init__.py
+++ src/py42/settings/__init__.py
@@ -11,16 +11,22 @@ verify_ssl_certs = True
 items_per_page = 500
 security_events_per_page = 500
 
+_custom_user_prefix = ""
 _custom_user_suffix = ""
 _python_version = f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
 
 
 def get_user_agent_string():
-    return "py42 {} python {}{}".format(
-        __version__, _python_version, _custom_user_suffix
+    return "{}py42/{} python/{}{}".format(
+        _custom_user_prefix, __version__, _python_version, _custom_user_suffix
     )
 
 
 def set_user_agent_suffix(suffix):
     global _custom_user_suffix
-    _custom_user_suffix = f" {suffix}"
+    _custom_user_suffix = f" {suffix}" if suffix else ""
+
+
+def set_user_agent_prefix(prefix):
+    global _custom_user_prefix
+    _custom_user_prefix = f"{prefix} " if prefix else ""

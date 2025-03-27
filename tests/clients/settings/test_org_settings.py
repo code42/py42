184cdbeb4a9f953eda148444a0b76ae0fb447c12
Andrew Orrison <andrew.orrison@crashplan.com>
2025-03-27 14:33:01 -0500

    reverted docstring

--- tests/clients/settings/test_org_settings.py
+++ tests/clients/settings/test_org_settings.py
@@ -148,7 +148,6 @@ def org_device_defaults_with_multiple_values(org_settings_dict):
 
 class TestOrgSettings:
     @pytest.mark.parametrize(
-        """PARAM DOC STRING TEST"""
         "param",
         [
             ("org_name", "TEST_ORG"),
